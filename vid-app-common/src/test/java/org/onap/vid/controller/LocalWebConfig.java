5554494dc573f17e6c77268319603f5f3dc68dd0
tgolabek <tomasz.golabek@nokia.com>
2018-05-23 11:40:17 +0200

    Refactor of an AAIRestInterface
    Refactor and some additional tests added
    
    (cherry picked from commit 6d8fa7d179b8de802ae386b317ddd1214eac1c47)
    
    Change-Id: Ibe7583353499352aa81d100b9995b9c74133c447
    Issue-ID: VID-229
    Signed-off-by: Stern, Ittay (is9613) <is9613@att.com>

--- vid-app-common/src/test/java/org/onap/vid/controller/LocalWebConfig.java
+++ vid-app-common/src/test/java/org/onap/vid/controller/LocalWebConfig.java
@@ -5,8 +5,7 @@ import org.onap.vid.aai.AaiClient;
 import org.onap.vid.aai.AaiClientInterface;
 import org.onap.vid.aai.AaiResponseTranslator;
 import org.onap.vid.aai.model.PortDetailsTranslator;
-import org.onap.vid.aai.util.AAIRestInterface;
-import org.onap.vid.aai.util.HttpsAuthClient;
+import org.onap.vid.aai.util.*;
 import org.onap.vid.asdc.AsdcClient;
 import org.onap.vid.asdc.parser.ToscaParserImpl2;
 import org.onap.vid.services.AaiService;
@@ -46,14 +45,29 @@ public class LocalWebConfig {
     }
 
     @Bean
-    public HttpsAuthClient httpsAuthClientFactory(ServletContext servletContext) {
+    public SSLContextProvider sslContextProvider() {
+        return new SSLContextProvider();
+    }
+
+    @Bean
+    public SystemPropertyHelper systemPropertyHelper() {
+        return new SystemPropertyHelper();
+    }
+
+    @Bean
+    public ServletRequestHelper servletRequestHelper() {
+        return new ServletRequestHelper();
+    }
+
+    @Bean
+    public HttpsAuthClient httpsAuthClientFactory(ServletContext servletContext, SystemPropertyHelper systemPropertyHelper, SSLContextProvider sslContextProvider) {
         final String certFilePath = new File(servletContext.getRealPath("/WEB-INF/cert/")).getAbsolutePath();
-        return new HttpsAuthClient(certFilePath);
+        return new HttpsAuthClient(certFilePath, systemPropertyHelper, sslContextProvider);
     }
 
     @Bean(name = "aaiRestInterface")
-    public AAIRestInterface aaiRestInterface(HttpsAuthClient httpsAuthClientFactory) {
-        return new AAIRestInterface(httpsAuthClientFactory);
+    public AAIRestInterface aaiRestInterface(HttpsAuthClient httpsAuthClientFactory, ServletRequestHelper servletRequestHelper, SystemPropertyHelper systemPropertyHelper) {
+        return new AAIRestInterface(httpsAuthClientFactory, servletRequestHelper, systemPropertyHelper);
     }
 
     @Bean
