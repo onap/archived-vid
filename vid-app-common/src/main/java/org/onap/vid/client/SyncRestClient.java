89f01790aee54b7bbf32f79650174c319f49b012
golabek <tomasz.golabek@nokia.com>
2018-08-01 15:56:44 +0200

    Generic REST client for external services
    Initial commit for generic REST client for requests to another components APIs
    
    Change-Id: I4af29ab1e45fd805403622e60585a351f3ef5d20
    Issue-ID: VID-266
    Signed-off-by: tgolabek <tomasz.golabek@nokia.com>

---
+++ vid-app-common/src/main/java/org/onap/vid/client/SyncRestClient.java
@@ -0,0 +1,174 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * VID
+ * ================================================================================
+ * Copyright (C) 2018 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.vid.client;
+
+import org.onap.portalsdk.core.logging.logic.EELFLoggerDelegate;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.onap.portalsdk.core.util.SystemProperties;
+import com.mashape.unirest.http.ObjectMapper;
+import com.mashape.unirest.http.HttpResponse;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.onap.vid.properties.VidProperties;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import org.apache.http.client.HttpClient;
+import io.vavr.CheckedFunction1;
+import lombok.SneakyThrows;
+import lombok.NonNull;
+import lombok.val;
+
+import java.security.cert.CertificateException;
+import javax.net.ssl.SSLHandshakeException;
+import java.text.SimpleDateFormat;
+import javax.net.ssl.SSLContext;
+import java.io.FileInputStream;
+import java.text.DateFormat;
+import java.io.IOException;
+import java.security.*;
+import java.util.Date;
+import java.util.Map;
+import java.io.File;
+
+public class SyncRestClient {
+
+    private static final String CANNOT_INITIALIZE_CUSTOM_HTTP_CLIENT = "Cannot initialize custom http client from current configuration. Using default one.";
+    private static final EELFLoggerDelegate logger = EELFLoggerDelegate.getLogger(HttpsBasicClient.class);
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss:SSSS");
+    private static final String[] SUPPORTED_PROTOCOLS = {"TLSv1", "TLSv1.2"};
+    private static final String HTTPS_SCHEMA = "https://";
+    private static final String HTTP_SCHEMA = "http://";
+
+    public SyncRestClient() {
+        initMappers();
+        Unirest.setHttpClient(initHttpClient());
+    }
+
+    public SyncRestClient(HttpClient httpClient) {
+        initMappers();
+        Unirest.setHttpClient(httpClient);
+    }
+
+    public HttpResponse<JsonNode> post(@NonNull String url, @NonNull Map<String, String> headers, @NonNull Object body) {
+        return callWithRetry(url, url2 -> Unirest.post(url2).headers(headers).body(body).asJson());
+    }
+
+    public <T> HttpResponse<T> post(@NonNull String url, @NonNull Map<String, String> headers, @NonNull Object body, @NonNull Class<T> aClass) {
+        return callWithRetry(url, url2 -> Unirest.post(url2).headers(headers).body(body).asObject(aClass));
+    }
+
+    public HttpResponse<JsonNode> get(@NonNull String url, @NonNull Map<String, String> headers, @NonNull Map<String, String> routeParams) {
+        return callWithRetry(url, url2 -> {
+            val getRequest = Unirest.get(url2).headers(headers);
+            routeParams.forEach(getRequest::routeParam);
+            return getRequest.asJson();
+        });
+    }
+
+    public <T> HttpResponse<T> get(@NonNull String url, @NonNull Map<String, String> headers, @NonNull Map<String, String> routeParams, @NonNull Class<T> aClass) {
+        return callWithRetry(url, url2 -> {
+            val getRequest = Unirest.get(url2).headers(headers);
+            routeParams.forEach(getRequest::routeParam);
+            return getRequest.asObject(aClass);
+        });
+    }
+
+    public HttpResponse<JsonNode> put(@NonNull String url, @NonNull Map<String, String> headers, @NonNull Object body) {
+        return callWithRetry(url, url2 -> Unirest.put(url2).headers(headers).body(body).asJson());
+    }
+
+    public <T> HttpResponse<T> put(@NonNull String url, @NonNull Map<String, String> headers, @NonNull Object body, @NonNull Class<T> aClass) {
+        return callWithRetry(url, url2 -> Unirest.put(url2).headers(headers).body(body).asObject(aClass));
+    }
+
+    @SneakyThrows
+    private <T> HttpResponse<T> callWithRetry(String url, CheckedFunction1<String, HttpResponse<T>> httpRequest) {
+        try {
+            return httpRequest.apply(url);
+        } catch (UnirestException e) {
+            if (e.getCause() instanceof SSLHandshakeException) {
+                return httpRequest.apply(url.replaceFirst(HTTPS_SCHEMA, HTTP_SCHEMA));
+            }
+            throw e;
+        }
+    }
+
+    private void initMappers() {
+        val objectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
+
+        Unirest.setObjectMapper(new ObjectMapper() {
+            @Override
+            @SneakyThrows
+            public <T> T readValue(String value, Class<T> aClass) {
+                return objectMapper.readValue(value, aClass);
+            }
+
+            @Override
+            @SneakyThrows
+            public String writeValue(Object value) {
+                return objectMapper.writeValueAsString(value);
+            }
+        });
+    }
+
+    private HttpClient initHttpClient() {
+        try {
+            val trustStorePath = SystemProperties.getProperty(VidProperties.VID_TRUSTSTORE_FILENAME);
+            val trustStorePass = SystemProperties.getProperty(VidProperties.VID_TRUSTSTORE_PASSWD_X);
+
+            val trustStore = loadTruststore(trustStorePath, trustStorePass);
+            val sslcontext = trustOwnCACerts(trustStorePass, trustStore);
+            val sslsf = allowTLSProtocols(sslcontext);
+
+            return HttpClients.custom().setSSLSocketFactory(sslsf).build();
+        } catch (IOException | CertificateException | UnrecoverableKeyException | NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {
+            logger.warn(EELFLoggerDelegate.debugLogger, DATE_FORMAT.format(new Date()) + CANNOT_INITIALIZE_CUSTOM_HTTP_CLIENT, e);
+            return HttpClients.createDefault();
+        }
+    }
+
+    private SSLConnectionSocketFactory allowTLSProtocols(SSLContext sslcontext) {
+        return new SSLConnectionSocketFactory(
+                sslcontext,
+                SUPPORTED_PROTOCOLS,
+                null,
+                SSLConnectionSocketFactory.STRICT_HOSTNAME_VERIFIER);
+    }
+
+    private SSLContext trustOwnCACerts(String trustStorePass, KeyStore trustStore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
+        return SSLContexts.custom()
+                .useTLS()
+                .loadKeyMaterial(trustStore, trustStorePass.toCharArray())
+                .loadTrustMaterial(trustStore, new TrustSelfSignedStrategy())
+                .build();
+    }
+
+    private KeyStore loadTruststore(String trustStorePath, String trustStorePass) throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException {
+        val trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        try (FileInputStream instream = new FileInputStream(new File(trustStorePath))) {
+            trustStore.load(instream, trustStorePass.toCharArray());
+        }
+        return trustStore;
+    }
+
+}
---
+++ vid-app-common/src/test/java/org/onap/vid/client/SyncRestClientForHttpsServerTest.java
@@ -0,0 +1,132 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * VID
+ * ================================================================================
+ * Copyright (C) 2018 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.vid.client;
+
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import com.xebialabs.restito.semantics.Condition;
+import com.mashape.unirest.http.HttpResponse;
+import com.xebialabs.restito.server.StubServer;
+import com.xebialabs.restito.semantics.Action;
+import org.apache.http.config.RegistryBuilder;
+import org.testng.annotations.BeforeMethod;
+import com.mashape.unirest.http.JsonNode;
+import org.testng.annotations.AfterMethod;
+import org.glassfish.grizzly.http.Method;
+import org.apache.http.client.HttpClient;
+import org.apache.http.config.Registry;
+import org.testng.annotations.Test;
+
+import java.security.GeneralSecurityException;
+import javax.net.ssl.SSLContext;
+import java.util.Collections;
+
+import static com.xebialabs.restito.builder.verify.VerifyHttp.verifyHttp;
+import static com.xebialabs.restito.builder.stub.StubHttp.whenHttp;
+import static com.xebialabs.restito.semantics.Action.stringContent;
+import static com.xebialabs.restito.semantics.Action.contentType;
+import static org.apache.http.client.config.RequestConfig.custom;
+import static com.xebialabs.restito.semantics.Action.ok;
+import static org.testng.Assert.assertEquals;
+
+public class SyncRestClientForHttpsServerTest {
+
+    private static final SyncRestClientModel.TestModel testObject = new SyncRestClientModel.TestModel(1, "test");
+    private static final String[] SUPPORTED_PROTOCOLS = {"TLSv1", "TLSv1.2"};
+    private StubServer stubServer;
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private SyncRestClient syncRestClient;
+
+    @BeforeMethod
+    public void setUp() throws GeneralSecurityException {
+        stubServer = new StubServer();
+        stubServer.secured().run();
+        syncRestClient = new SyncRestClient(createNaiveHttpClient());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        stubServer.stop();
+    }
+
+    @Test
+    public void testJsonResponseFromGet() throws JsonProcessingException {
+        // given
+        stubGetCall();
+        String securedUrl = "https://0.0.0.0:" + stubServer.getPort() + "/test";
+        String notSecuredUrl = "http://0.0.0.0:" + stubServer.getPort() + "/test";
+        // when
+        HttpResponse<JsonNode> jsonNodeHttpResponse = syncRestClient.get(securedUrl, Collections.emptyMap(), Collections.emptyMap());
+        // then
+        verifyHttp(stubServer).once(Condition.method(Method.GET), Condition.url(securedUrl), Condition.not(Condition.url(notSecuredUrl)));
+        assertEquals(jsonNodeHttpResponse.getStatus(), 200);
+        assertEquals(jsonNodeHttpResponse.getBody().getObject().get("key"), 1);
+        assertEquals(jsonNodeHttpResponse.getBody().getObject().get("value"), "test");
+    }
+
+    @Test
+    public void testObjectResponseFromGet() throws JsonProcessingException {
+        // given
+        stubServer.run();
+        stubGetCall();
+        String securedUrl = "https://0.0.0.0:" + stubServer.getPort() + "/test";
+        String notSecuredUrl = "http://0.0.0.0:" + stubServer.getPort() + "/test";
+        // when
+        HttpResponse<SyncRestClientModel.TestModel> testModelHttpResponse = syncRestClient.get(securedUrl, Collections.emptyMap(), Collections.emptyMap(), SyncRestClientModel.TestModel.class);
+        // then
+        verifyHttp(stubServer).once(Condition.method(Method.GET), Condition.url(securedUrl), Condition.not(Condition.url(notSecuredUrl)));
+        assertEquals(testModelHttpResponse.getStatus(), 200);
+        assertEquals(testModelHttpResponse.getBody().getKey(), 1);
+        assertEquals(testModelHttpResponse.getBody().getValue(), "test");
+    }
+
+    private Action jsonContent() throws JsonProcessingException {
+        return stringContent(objectMapper.writeValueAsString(testObject));
+    }
+
+    private void stubGetCall() throws JsonProcessingException {
+        whenHttp(stubServer)
+                .match(Condition.get("/test"))
+                .then(ok(), jsonContent(), contentType("application/json"));
+    }
+
+    private HttpClient createNaiveHttpClient() throws GeneralSecurityException {
+        final SSLContext context = new SSLContextBuilder().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
+
+        final SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(context, SUPPORTED_PROTOCOLS, null, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("https", socketFactory)
+                .build();
+
+        return HttpClientBuilder.create()
+                .setDefaultRequestConfig(custom().setConnectionRequestTimeout(10000).build())
+                .setConnectionManager(new PoolingHttpClientConnectionManager(registry))
+                .setSSLSocketFactory(socketFactory).build();
+    }
+
+}
