5dd5198477439a5cac607508cf7243c4b65b4cef
koblosz <sandra.koblosz@nokia.com>
2018-08-20 16:42:57 +0200

    VID-266 added stub server for testing
    Change-Id: I39b8fca2df4c45123e2308142dfbb43c737f7707
    Issue-ID: VID-266
    Signed-off-by: Koblosz, Sandra <sandra.koblosz@nokia.com>

--- vid-app-common/src/main/java/org/onap/vid/scheduler/SchedulerRestInterface.java
+++ vid-app-common/src/main/java/org/onap/vid/scheduler/SchedulerRestInterface.java
@@ -18,123 +18,134 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.Response;
 import java.util.Collections;
+import java.util.function.Function;
 
 import static org.onap.vid.utils.Logging.REQUEST_ID_HEADER_KEY;
 
 @Service
 public class SchedulerRestInterface implements SchedulerRestInterfaceIfc {
 
-	private Client client = null;
+    private Client client = null;
+    private Function<String, String> propertyGetter;
 
-	@Autowired
-	HttpsAuthClient httpsAuthClient;
+    @Autowired
+    HttpsAuthClient httpsAuthClient;
 
-	private MultivaluedHashMap<String, Object> commonHeaders;
-	
-	/** The logger. */
-	private static EELFLoggerDelegate logger = EELFLoggerDelegate.getLogger(SchedulerRestInterface.class);
-	final private static EELFLogger outgoingRequestsLogger = Logging.getRequestsLogger("scheduler");
-	
-	public void initRestClient() {
-		logger.info("Starting to initialize rest client ");
-		
-		final String username;
-		final String password;
-		
+    private MultivaluedHashMap<String, Object> commonHeaders;
+
+    /** The logger. */
+    private static EELFLoggerDelegate logger = EELFLoggerDelegate.getLogger(SchedulerRestInterface.class);
+    final private static EELFLogger outgoingRequestsLogger = Logging.getRequestsLogger("scheduler");
+
+    @Autowired
+    public SchedulerRestInterface(){
+        this.propertyGetter = SystemProperties::getProperty;
+    }
+
+    public SchedulerRestInterface(Function<String, String> propertyGetter){
+        this.propertyGetter = propertyGetter;
+    }
+
+    public void initRestClient() {
+        logger.info("Starting to initialize rest client ");
+
+        final String username;
+        final String password;
+
 		/*Setting user name based on properties*/
-		String retrievedUsername = SystemProperties.getProperty(SchedulerProperties.SCHEDULER_USER_NAME_VAL);
-		if(retrievedUsername.isEmpty()) {
-			username = "";
-		} else {
-			username = retrievedUsername;
-		}
-		
+        String retrievedUsername = propertyGetter.apply(SchedulerProperties.SCHEDULER_USER_NAME_VAL);
+        if(retrievedUsername.isEmpty()) {
+            username = "";
+        } else {
+            username = retrievedUsername;
+        }
+
 		/*Setting password based on properties*/
-		String retrievedPassword = SystemProperties.getProperty(SchedulerProperties.SCHEDULER_PASSWORD_VAL);
-		if(retrievedPassword.isEmpty()) {
-			password = "";
-		} else {
-			if (retrievedPassword.contains("OBF:")) {
-				password = Password.deobfuscate(retrievedPassword);
-			} else {
-				password = retrievedPassword;
-			}
-		}
-		
-		String authString = username + ":" + password;
-				
-		byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
-		String authStringEnc = new String(authEncBytes);
+        String retrievedPassword = propertyGetter.apply(SchedulerProperties.SCHEDULER_PASSWORD_VAL);
+        if(retrievedPassword.isEmpty()) {
+            password = "";
+        } else {
+            if (retrievedPassword.contains("OBF:")) {
+                password = Password.deobfuscate(retrievedPassword);
+            } else {
+                password = retrievedPassword;
+            }
+        }
 
-		commonHeaders = new MultivaluedHashMap<String, Object> ();
-		commonHeaders.put("Authorization", Collections.singletonList("Basic " + authStringEnc));
-					
-		try {
-			if ( !username.isEmpty() ) { 
-				client = httpsAuthClient.getClient(HttpClientMode.WITHOUT_KEYSTORE);
-			}
-			else {
-				
-				client = HttpBasicClient.getClient();
-			}
-		} catch (Exception e) {
-			logger.error(" <== Unable to initialize rest client ", e);
-		}
+        String authString = username + ":" + password;
 
-		logger.info("\t<== Client Initialized \n");
-	}
-		
-	public <T> void Get (T t, String sourceId, String path, org.onap.vid.scheduler.RestObject<T> restObject ) {
-		
-		String methodName = "Get";
-		String url = SystemProperties.getProperty(SchedulerProperties.SCHEDULER_SERVER_URL_VAL) + path;
-		initRestClient();
-		Logging.logRequest(outgoingRequestsLogger, HttpMethod.GET, url);
-		final Response cres = client.target(url)
-			 .request()
-	         .accept("application/json")
-	         .headers(commonHeaders)
-			 .header(REQUEST_ID_HEADER_KEY, Logging.extractOrGenerateRequestId())
-			 .get();
-		Logging.logResponse(outgoingRequestsLogger, HttpMethod.GET, url, cres);
-		int status = cres.getStatus();
-		restObject.setStatusCode (status);
-		
-		if (status == 200) {
-			 t = (T) cres.readEntity(t.getClass());
-			 restObject.set(t);
-			
-		 } else {
-		     throw new GenericUncheckedException(methodName + " with status="+ status + ", url= " + url );
-		 }
+        byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+        String authStringEnc = new String(authEncBytes);
 
-	}
+        commonHeaders = new MultivaluedHashMap<> ();
+        commonHeaders.put("Authorization", Collections.singletonList("Basic " + authStringEnc));
 
-	public <T> void Delete(T t, String sourceID, String path, org.onap.vid.scheduler.RestObject<T> restObject) {
+        try {
+            if ( !username.isEmpty() ) {
+                client = httpsAuthClient.getClient(HttpClientMode.WITHOUT_KEYSTORE);
+            }
+            else {
 
-		initRestClient();
+                client = HttpBasicClient.getClient();
+            }
+        } catch (Exception e) {
+            logger.error(" <== Unable to initialize rest client ", e);
+        }
 
-		String url = SystemProperties.getProperty(SchedulerProperties.SCHEDULER_SERVER_URL_VAL) + path;
-		Logging.logRequest(outgoingRequestsLogger, HttpMethod.DELETE, url);
-		Response cres = client.target(url)
-				.request()
-				.accept("application/json")
-				.headers(commonHeaders)
-				.header(REQUEST_ID_HEADER_KEY, Logging.extractOrGenerateRequestId())
-				.delete();
-		Logging.logResponse(outgoingRequestsLogger, HttpMethod.DELETE, url, cres);
+        logger.info("\t<== Client Initialized \n");
+    }
 
-		int status = cres.getStatus();
-		restObject.setStatusCode(status);
+    public <T> void Get (T t, String sourceId, String path, org.onap.vid.scheduler.RestObject<T> restObject ) {
 
-		t = (T) cres.readEntity(t.getClass());
-		restObject.set(t);
+        String methodName = "Get";
+        String url = String.format("%s%s",propertyGetter.apply(SchedulerProperties.SCHEDULER_SERVER_URL_VAL), path);
+        initRestClient();
+        Logging.logRequest(outgoingRequestsLogger, HttpMethod.GET, url);
+        final Response cres = client.target(url)
+                .request()
+                .accept("application/json")
+                .headers(commonHeaders)
+                .header(REQUEST_ID_HEADER_KEY, Logging.extractOrGenerateRequestId())
+                .get();
+        Logging.logResponse(outgoingRequestsLogger, HttpMethod.GET, url, cres);
+        int status = cres.getStatus();
+        restObject.setStatusCode (status);
 
-	}
+        if (status == 200) {
+            t = (T) cres.readEntity(t.getClass());
+            restObject.set(t);
 
-	public <T> T getInstance(Class<T> clazz) throws IllegalAccessException, InstantiationException
-	{
-		return clazz.newInstance();
-	}
+        } else {
+            throw new GenericUncheckedException(String.format("%s with status=%d, url=%s", methodName, status,url));
+        }
+
+    }
+
+    public <T> void Delete(T t, String sourceID, String path, org.onap.vid.scheduler.RestObject<T> restObject) {
+
+        initRestClient();
+
+        String url = String.format( "%s%s",propertyGetter.apply(SchedulerProperties.SCHEDULER_SERVER_URL_VAL), path);
+        Logging.logRequest(outgoingRequestsLogger, HttpMethod.DELETE, url);
+        Response cres = client.target(url)
+                .request()
+                .accept("application/json")
+                .headers(commonHeaders)
+                .header(REQUEST_ID_HEADER_KEY, Logging.extractOrGenerateRequestId())
+                .delete();
+        Logging.logResponse(outgoingRequestsLogger, HttpMethod.DELETE, url, cres);
+
+        int status = cres.getStatus();
+        restObject.setStatusCode(status);
+
+        t = (T) cres.readEntity(t.getClass());
+        restObject.set(t);
+
+    }
+
+    public <T> T getInstance(Class<T> clazz) throws IllegalAccessException, InstantiationException
+    {
+        return clazz.newInstance();
+    }
 
 }
