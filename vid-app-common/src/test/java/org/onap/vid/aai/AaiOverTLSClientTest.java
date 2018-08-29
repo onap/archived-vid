4353dd523510c4e2e8d774c12481eb141acc2c4e
golabek <tomasz.golabek@nokia.com>
2018-08-29 15:29:56 +0200

    Nodes query rewritten to generic rest client
    Added AAI Rest Client based on new generic client
    
    Change-Id: I561f8a5d95ec35fdddc648b24965dd9dac03c9a4
    Issue-ID: VID-269
    Signed-off-by: golabek <tomasz.golabek@nokia.com>

---
+++ vid-app-common/src/test/java/org/onap/vid/aai/AaiOverTLSClientTest.java
@@ -0,0 +1,55 @@
+package org.onap.vid.aai;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Collections;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.onap.vid.aai.model.AaiNodeQueryResponse;
+import org.onap.vid.aai.model.ResourceType;
+import org.onap.vid.client.SyncRestClient;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AaiOverTLSClientTest {
+
+    public static final String SEARCH_NODES_QUERY_SEARCH_NODE_TYPE = "search/nodes-query?search-node-type=generic-vnf&filter=vnf-name:EQUALS:name";
+    private AaiOverTLSClient aaiRestClient;
+
+    @Mock
+    private SyncRestClient syncRestClient;
+    @Mock
+    private AaiOverTLSPropertySupplier propertySupplier;
+
+    @Before
+    public void setUp() {
+        aaiRestClient = new AaiOverTLSClient(syncRestClient,  propertySupplier);
+    }
+
+    @Test
+    public void searchNodeTypeByName() {
+        mockPropertyReader();
+
+        aaiRestClient.searchNodeTypeByName("name", ResourceType.GENERIC_VNF);
+        Mockito.verify(syncRestClient).get(Matchers.contains(SEARCH_NODES_QUERY_SEARCH_NODE_TYPE),
+            Matchers.eq(getHeaders()), Matchers.eq(Collections.emptyMap()), Matchers.eq(AaiNodeQueryResponse.class));
+    }
+
+    private void mockPropertyReader() {
+        Mockito.when(propertySupplier.getPassword()).thenReturn("Pass");
+        Mockito.when(propertySupplier.getUsername()).thenReturn("User");
+        Mockito.when(propertySupplier.getRequestId()).thenReturn("1");
+        Mockito.when(propertySupplier.getRandomUUID()).thenReturn("2");
+    }
+
+    private Map<String,String> getHeaders(){
+        return ImmutableMap.<String, String>builder().put("Authorization", "Basic VXNlcjpQYXNz").
+            put("X-FromAppId", "VidAaiController").put("Accept", "application/json").put("X-ECOMP-RequestID", "1").
+            put("X-TransactionId", "2").put("Content-Type", "application/json").build();
+    }
+
+}
