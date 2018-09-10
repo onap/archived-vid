b4aa71e35ea1fc3898fc1577fd4c8f7a228f34ca
Jakub Zieba <jakub.zieba@nokia.com>
2018-09-10 08:35:18 +0200

    Use correct trust store
    Use correct trust store file in SSL configuration
    
    Fix for schema and data creation
    
    Change-Id: Ie5b329191779a75ea902f547f09b59b65f09f64c
    Issue-ID: VID-271
    Signed-off-by: Jakub Zieba <jakub.zieba@nokia.com>

--- epsdk-app-onap/src/main/java/org/onap/portalapp/conf/ExternalAppConfig.java
+++ epsdk-app-onap/src/main/java/org/onap/portalapp/conf/ExternalAppConfig.java
@@ -57,6 +57,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
@@ -180,6 +181,7 @@ public class ExternalAppConfig extends AppConfig implements Configurable {
 	 */
 	@Bean // ANNOTATION COMMENTED OUT
 	// APPLICATIONS REQUIRING QUARTZ SHOULD RESTORE ANNOTATION
+	@DependsOn("dataSourceInitializer")
 	public SchedulerFactoryBean schedulerFactoryBean() throws Exception {
 		SchedulerFactoryBean scheduler = new SchedulerFactoryBean();
 		scheduler.setTriggers(schedulerRegistryAdapter.getTriggers());
