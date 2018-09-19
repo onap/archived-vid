d578454e8147f41db7fbeabfa7bf2f4f8ef80527
Joanna Jeremicz <joanna.jeremicz@nokia.com>
2018-09-19 14:35:00 +0200

    HealthCheckController/test refactor
    Issue-ID: VID-312
    Change-Id: Iabdb91c9b4940ff7a173656819ad5fa807a734e9
    Signed-off-by: Joanna Jeremicz <joanna.jeremicz@nokia.com>

--- vid-app-common/src/main/java/org/onap/vid/controllers/HealthCheckController.java
+++ vid-app-common/src/main/java/org/onap/vid/controllers/HealthCheckController.java
@@ -25,7 +25,6 @@ import org.onap.portalsdk.core.logging.logic.EELFLoggerDelegate;
 import org.onap.portalsdk.core.util.SystemProperties;
 import org.onap.vid.dao.FnAppDoaImpl;
 import org.onap.vid.model.GitRepositoryState;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,39 +46,42 @@ import java.util.Properties;
 @RequestMapping("/")
 public class HealthCheckController extends UnRestrictedBaseController {
 
-    /**
-     * The logger.
-     */
     private static final EELFLoggerDelegate LOGGER = EELFLoggerDelegate.getLogger(HealthCheckController.class);
-
-    /**
-     * The Constant dateFormat.
-     */
-    final static DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss:SSSS");
-
-    private static final String HEALTH_CHECK_PATH = "/healthCheck";
+    private static final DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss:SSSS");
     private static final String GIT_PROPERTIES_FILENAME = "git.properties";
+
+    private enum StatusCode {
+        SUCCESS(200),
+        FAILURE(500);
+
+        private int value;
+
+        StatusCode(int i) {
+            value = i;
+        }
+
+        public int getValue(){
+            return value;
+        }
+    }
 
     /**
      * Model for JSON response with health-check results.
      */
-    public class HealthStatus {
-        // Either 200 or 500
-        public int statusCode;
+    public final class HealthStatus {
 
-        // Additional detail in case of error, empty in case of success.
-        public String message;
+        private int statusCode;
+        private String detail;
+        private String date;
 
-        public String date;
-
-        public HealthStatus(int code, String msg) {
-            this.statusCode = code;
-            this.message = msg;
+        public HealthStatus(StatusCode code, String msg) {
+            this.statusCode = code.getValue();
+            this.detail = msg;
         }
 
-        public HealthStatus(int code, String date, String msg) {
-            this.statusCode = code;
-            this.message = msg;
+        public HealthStatus(StatusCode code, String date, String msg) {
+            this.statusCode = code.getValue();
+            this.detail = msg;
             this.date = date;
         }
 
@@ -87,33 +89,18 @@ public class HealthCheckController extends UnRestrictedBaseController {
             return statusCode;
         }
 
-        public void setStatusCode(int code) {
-            this.statusCode = code;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String msg) {
-            this.message = msg;
+        public String getDetail() {
+            return detail;
         }
 
         public String getDate() {
             return date;
         }
-
-        public void setDate(String date) {
-            this.date = date;
-        }
-
     }
 
     @SuppressWarnings("unchecked")
     public int getProfileCount(String driver, String URL, String username, String password) {
-        FnAppDoaImpl doa = new FnAppDoaImpl();
-        int count = doa.getProfileCount(driver, URL, username, password);
-        return count;
+        return new FnAppDoaImpl().getProfileCount(driver, URL, username, password);
     }
 
 
@@ -122,7 +109,6 @@ public class HealthCheckController extends UnRestrictedBaseController {
      * Used by IDNS for redundancy
      *
      * @return ResponseEntity The response entity
-     * @throws IOException Signals that an I/O exception has occurred.
      */
     @RequestMapping(value = "/healthCheck", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public HealthStatus gethealthCheckStatusforIDNS() {
@@ -137,19 +123,16 @@ public class HealthCheckController extends UnRestrictedBaseController {
         LOGGER.debug(EELFLoggerDelegate.debugLogger, "username::" + username);
         LOGGER.debug(EELFLoggerDelegate.debugLogger, "password::" + password);
 
-
-        HealthStatus healthStatus = null;
         try {
             LOGGER.debug(EELFLoggerDelegate.debugLogger, "Performing health check");
             int count = getProfileCount(driver, URL, username, password);
             LOGGER.debug(EELFLoggerDelegate.debugLogger, "count:::" + count);
-            healthStatus = new HealthStatus(200, "health check succeeded");
+            return new HealthStatus(StatusCode.SUCCESS, "health check succeeded");
         } catch (Exception ex) {
 
             LOGGER.error(EELFLoggerDelegate.errorLogger, "Failed to perform health check", ex);
-            healthStatus = new HealthStatus(500, "health check failed: " + ex.toString());
+            return new HealthStatus(StatusCode.FAILURE, "health check failed: " + ex.toString());
         }
-        return healthStatus;
     }
 
     /**
@@ -174,8 +157,6 @@ public class HealthCheckController extends UnRestrictedBaseController {
         LOGGER.debug(EELFLoggerDelegate.debugLogger, "username::" + username);
         LOGGER.debug(EELFLoggerDelegate.debugLogger, "password::" + password);
 
-
-        HealthStatus healthStatus = null;
         try {
             LOGGER.debug(EELFLoggerDelegate.debugLogger, "Performing health check");
             LOGGER.debug(EELFLoggerDelegate.debugLogger, "User-Agent" + UserAgent);
@@ -185,13 +166,12 @@ public class HealthCheckController extends UnRestrictedBaseController {
             int count = getProfileCount(driver, URL, username, password);
 
             LOGGER.debug(EELFLoggerDelegate.debugLogger, "count:::" + count);
-            healthStatus = new HealthStatus(200, dateFormat.format(new Date()), "health check succeeded");
+            return new HealthStatus(StatusCode.SUCCESS, dateFormat.format(new Date()), "health check succeeded");
         } catch (Exception ex) {
 
             LOGGER.error(EELFLoggerDelegate.errorLogger, "Failed to perform health check", ex);
-            healthStatus = new HealthStatus(500, dateFormat.format(new Date()), "health check failed: " + ex.toString());
+            return new HealthStatus(StatusCode.FAILURE, dateFormat.format(new Date()), "health check failed: " + ex.toString());
         }
-        return healthStatus;
     }
 
     @RequestMapping(value = "/version", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
