f99fc73a1ee0505202c1645f33ecdc25f4e802e7
Joanna Jeremicz <joanna.jeremicz@nokia.com>
2018-09-19 14:35:00 +0200

    HealthCheckController/test refactor
    Issue-ID: VID-312
    Change-Id: Iabdb91c9b4940ff7a173656819ad5fa807a734e9
    Signed-off-by: Joanna Jeremicz <joanna.jeremicz@nokia.com>

--- vid-app-common/src/test/java/org/onap/vid/controllers/HealthCheckControllerTest.java
+++ vid-app-common/src/test/java/org/onap/vid/controllers/HealthCheckControllerTest.java
@@ -3,7 +3,6 @@ package org.onap.vid.controllers;
 import org.apache.log4j.BasicConfigurator;
 import org.junit.Before;
 import org.junit.Test;
-import org.onap.vid.controllers.HealthCheckController.HealthStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -15,53 +14,42 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class HealthCheckControllerTest {
 
-	private HealthCheckController testSubject;
-	private MockMvc mockMvc;
+    private HealthCheckController testSubject;
+    private MockMvc mockMvc;
 
-	@Before
-	public void setUp() {
-		testSubject = new HealthCheckController();
-		BasicConfigurator.configure();
-		mockMvc = MockMvcBuilders.standaloneSetup(testSubject).build();
-	}
+    @Before
+    public void setUp() {
+        testSubject = new HealthCheckController();
+        BasicConfigurator.configure();
+        mockMvc = MockMvcBuilders.standaloneSetup(testSubject).build();
+    }
 
-	@Test
-	public void testGetProfileCount() throws Exception {
-		String driver = "";
-		String URL = "";
-		String username = "";
-		String password = "";
-		int result;
+    @Test
+    public void testGethealthCheckStatusforIDNS() throws Exception {
+        mockMvc.perform(get("/healthCheck")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value(200))
+                .andExpect(jsonPath("$.message").value("health check succeeded"));
+    }
 
-		// default test
-		result = testSubject.getProfileCount(driver, URL, username, password);
-	}
+    @Test
+    public void testGetHealthCheck() throws Exception {
+        mockMvc.perform(get("/rest/healthCheck/{User-Agent}/{X-ECOMP-RequestID}", "userAgent", "requestId")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value(200))
+                .andExpect(jsonPath("$.message").value("health check succeeded"))
+                .andExpect(jsonPath("$.date").isString());
+    }
 
-	@Test
-	public void testGethealthCheckStatusforIDNS() throws Exception {
-		HealthStatus result;
-
-		// default test
-		result = testSubject.gethealthCheckStatusforIDNS();
-	}
-
-	@Test
-	public void testGetHealthCheck() throws Exception {
-		String UserAgent = "";
-		String ECOMPRequestID = "";
-		HealthStatus result;
-
-		// default test
-		result = testSubject.getHealthCheck(UserAgent, ECOMPRequestID);
-	}
-
-	@Test
-	public void testCommitInfoEndpoint() throws Exception {
-		mockMvc.perform(get("/version")
-				.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.commitId").value("123987"))
-				.andExpect(jsonPath("$.commitMessageShort").value("Test short commit message"))
-				.andExpect(jsonPath("$.commitTime").value("1999-09-12T13:48:55+0200"));
-	}
+    @Test
+    public void testCommitInfoEndpoint() throws Exception {
+        mockMvc.perform(get("/version")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.commitId").value("123"))
+                .andExpect(jsonPath("$.commitMessageShort").value("Test short commit message"))
+                .andExpect(jsonPath("$.commitTime").value("1999-09-12T13:48:55+0200"));
+    }
 }
