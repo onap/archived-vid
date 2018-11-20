7aba15604952cd97767144fb871dde88884c805f
Joanna Jeremicz <joanna.jeremicz@nokia.com>
2018-11-20 08:52:31 +0100

    ChangeManagementController tests
    Change-Id: I936a9c65c167e5076d775f4139cc76340d0fc55d
    Issue-ID: VID-353
    Signed-off-by: Joanna Jeremicz <joanna.jeremicz@nokia.com>

--- vid-app-common/src/main/java/org/onap/vid/changeManagement/ChangeManagementRequest.java
+++ vid-app-common/src/main/java/org/onap/vid/changeManagement/ChangeManagementRequest.java
@@ -1,3 +1,25 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * VID
+ * ================================================================================
+ * Copyright © 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Modifications Copyright 2018 Nokia
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
 package org.onap.vid.changeManagement;
 
 import java.util.List;
@@ -8,6 +30,8 @@ import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 @JsonPropertyOrder({
     "requestDetails",
@@ -69,6 +93,29 @@ public class ChangeManagementRequest {
 	this.additionalProperties.put(name, value);
 	}
 
-	
-	
+	@Override
+	public boolean equals(Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (other == null || this.getClass() != other.getClass()) {
+			return false;
+		}
+		ChangeManagementRequest request = (ChangeManagementRequest) other;
+
+		return new EqualsBuilder()
+			.append(getRequestDetails(), request.getRequestDetails())
+			.append(getRequestType(), request.getRequestType())
+			.append(getAdditionalProperties(), request.getAdditionalProperties())
+			.isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37)
+			.append(getRequestDetails())
+			.append(getRequestType())
+			.append(getAdditionalProperties())
+			.toHashCode();
+	}
 }
--- vid-app-common/src/main/java/org/onap/vid/changeManagement/VnfWorkflowRelationRequest.java
+++ vid-app-common/src/main/java/org/onap/vid/changeManagement/VnfWorkflowRelationRequest.java
@@ -1,8 +1,32 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * VID
+ * ================================================================================
+ * Copyright © 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Modifications Copyright 2018 Nokia
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
 package org.onap.vid.changeManagement;
 
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 public class VnfWorkflowRelationRequest {
 
@@ -25,7 +49,22 @@ public class VnfWorkflowRelationRequest {
 	public void setWorkflowsDetails(List<WorkflowsDetail> workflowsDetails) {
 		this.workflowsDetails = workflowsDetails;
 	}
-	
-	
 
+	@Override
+	public boolean equals(Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (other == null || this.getClass() != other.getClass()) {
+			return false;
+		}
+		VnfWorkflowRelationRequest request = (VnfWorkflowRelationRequest) other;
+
+		return new EqualsBuilder().append(getWorkflowsDetails(), request.getWorkflowsDetails()).isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37).append(getWorkflowsDetails()).toHashCode();
+	}
 }
