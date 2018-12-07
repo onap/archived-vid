5aa466249417ef8f8b7fd948efc3c1f30134f70a
biniek <lukasz.biniek@nokia.com>
2018-12-07 17:50:35 +0100

    Refactor of Intersection class
    Change-Id: Ida73b13b1e235951f01888ac0cbf344565976e0f
    Issue-ID: VID-364
    Signed-off-by: biniek <lukasz.biniek@nokia.com>

--- vid-app-common/src/main/java/org/onap/vid/utils/Intersection.java
+++ vid-app-common/src/main/java/org/onap/vid/utils/Intersection.java
@@ -1,31 +1,44 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * VID
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2018 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
 package org.onap.vid.utils;
 
+import com.google.common.collect.Sets;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
-/**
- * Created by moriya1 on 10/10/2017.
- */
-public class Intersection<T> {
-    public List<T> intersectMultipileArray(List<List<T>> lists) {
-        if (lists.size() == 1) {
-            return lists.get(0);
-        } else {
-            List<T> intersectResult = intersectTwoArrays(lists.get(0),lists.get(1));
 
-            lists.remove(0);
-            lists.remove(0);
-            lists.add(0,intersectResult);
-            return intersectMultipileArray(lists);
-        }
-
+public class Intersection {
+    private Intersection() {
     }
 
-    public List<T> intersectTwoArrays(List<T> list1, List<T> list2) {
-
-        List<T> intersect = list1.stream()
-                .filter(list2::contains)
-                .collect(Collectors.toList());
-        return intersect;
+    public static <T> List<T> of(List<List<T>> lists) {
+        return new ArrayList<>(lists.stream()
+                .map(elements -> (Set<T>) Sets.newHashSet(elements))
+                .reduce((a, b) -> a.stream().filter(b::contains).collect(Collectors.toSet()))
+                .orElse(Collections.emptySet())
+        );
     }
 }
