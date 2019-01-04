0343c5dea48df3cb8fb386ce44a8183d254085c9
biniek <lukasz.biniek@nokia.com>
2019-01-04 16:21:41 +0100

    MsoUtil refactor
    Change-Id: I778d60064508aa47e1225d0dbae38ca1100d248f
    Issue-ID: VID-368
    Signed-off-by: biniek <lukasz.biniek@nokia.com>

--- vid-app-common/src/main/java/org/onap/vid/mso/MsoUtil.java
+++ vid-app-common/src/main/java/org/onap/vid/mso/MsoUtil.java
@@ -3,13 +3,14 @@
  * VID
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2018 Nokia. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,111 +21,34 @@
 
 package org.onap.vid.mso;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.joshworks.restclient.http.HttpResponse;
-import org.apache.commons.lang3.ObjectUtils;
-import org.glassfish.jersey.client.ClientResponse;
-import org.onap.portalsdk.core.logging.logic.EELFLoggerDelegate;
 
-import static org.onap.vid.utils.Logging.getMethodName;
+import java.util.Objects;
 
-/**
- * The Class MsoUtil.
- */
 public class MsoUtil {
-	
-	/** The logger. */
-	private static final EELFLoggerDelegate logger = EELFLoggerDelegate.getLogger(MsoUtil.class);
 
-	/**
-	 * Wrap response.
-	 *
-	 * @param body the body
-	 * @param statusCode the status code
-	 * @return the mso response wrapper
-	 */
-	public static MsoResponseWrapper wrapResponse ( String body, int statusCode ) {
-		
-		MsoResponseWrapper w = new MsoResponseWrapper();
-		w.setStatus (statusCode);
-		w.setEntity(body);
-		
-		return w;
-	}
+    private MsoUtil() {
+    }
 
-	/**
-	 * Wrap response.
-	 *
-	 * @param cres the cres
-	 * @return the mso response wrapper
-	 */
-	public static MsoResponseWrapper wrapResponse (ClientResponse cres) {	
-		String respStr = "";
-		int statuscode = 0;
-		if ( cres != null ) {
-			respStr = cres.readEntity(String.class);
-			statuscode = cres.getStatus();
-		}
-		MsoResponseWrapper w = MsoUtil.wrapResponse ( respStr, statuscode );
-		return (w);
-	}
-	
-	/**
-	 * Wrap response.
-	 *
-	 * @param rs the rs
-	 * @return the mso response wrapper
-	 */
-	public static MsoResponseWrapper wrapResponse (RestObject<String> rs) {
-		String respStr = null;
-		int status = 0;
-		if ( rs != null ) {
-			respStr = rs.get() != null ? rs.get() : rs.getRaw();
-			status = rs.getStatusCode();
-		}
-		MsoResponseWrapper w = MsoUtil.wrapResponse ( respStr, status );
-		return (w);
-	}	
-	
-	public static <T> MsoResponseWrapper wrapResponse (HttpResponse<T> rs) {
-		MsoResponseWrapper w = new MsoResponseWrapper();
-		w.setStatus (rs.getStatus());
-		if(rs.getRawBody() != null) {
-			w.setEntity(ObjectUtils.toString(rs.getBody()));
-		}
-		return w;
-	}
+    private static MsoResponseWrapper wrapResponse(String body, int statusCode) {
+        MsoResponseWrapper wrapper = new MsoResponseWrapper();
+        wrapper.setStatus(statusCode);
+        wrapper.setEntity(body);
+        return wrapper;
+    }
 
-	/**
-	 * Convert pojo to string.
-	 *
-	 * @param <T> the generic type
-	 * @param t the t
-	 * @return the string
-	 * @throws JsonProcessingException the json processing exception
-	 */
-	public static <T> String convertPojoToString ( T t ) {
-		ObjectMapper mapper = new ObjectMapper();
-		String rJsonStr = "";
-	    if ( t != null ) {
-		    try {
-		    	rJsonStr = mapper.writeValueAsString(t);
-		    }
-		    catch ( com.fasterxml.jackson.core.JsonProcessingException j ) {
-		    	logger.debug(EELFLoggerDelegate.debugLogger,getMethodName() + " Unable to parse object of type " + t.getClass().getName() + " as json", j);
-		    }
-	    }
-	    return (rJsonStr);
-	}
-	
-	/**
-	 * The main method.
-	 *
-	 * @param args the arguments
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static MsoResponseWrapper wrapResponse(RestObject<String> restObject) {
+        String response = restObject.get() != null ? restObject.get() : restObject.getRaw();
+        int status = restObject.getStatusCode();
+        return MsoUtil.wrapResponse(response, status);
+    }
 
-	}
+    public static <T> MsoResponseWrapper wrapResponse(HttpResponse<T> httpResponse) {
+        MsoResponseWrapper msoResponseWrapper = new MsoResponseWrapper();
+        msoResponseWrapper.setStatus(httpResponse.getStatus());
+        if (httpResponse.getRawBody() != null) {
+            msoResponseWrapper.setEntity(Objects.toString(httpResponse.getBody()));
+        }
+        return msoResponseWrapper;
+    }
 }
