36d29e46fbc50439e5e7a62efa08505bb2d047f8
Joanna Jeremicz <joanna.jeremicz@nokia.com>
2019-01-18 11:17:46 +0100

    Rewrite PropertyControllerTest
    Change-Id: I8c564a945f374d6dcc12a30ff4d8d9268b50a815
    Issue-ID: VID-384
    Signed-off-by: Joanna Jeremicz <joanna.jeremicz@nokia.com>

--- vid-app-common/src/test/java/org/onap/vid/controller/PropertyControllerTest.java
+++ vid-app-common/src/test/java/org/onap/vid/controller/PropertyControllerTest.java
@@ -1,39 +1,139 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * VID
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Modifications Copyright 2019 Nokia
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
 package org.onap.vid.controller;
 
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.servlet.ModelAndView;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.onap.vid.category.CategoryParameterOptionRep;
+import org.onap.vid.category.CategoryParametersResponse;
+import org.onap.vid.model.CategoryParameter.Family;
+import org.onap.vid.services.CategoryParameterService;
+import org.onap.vid.utils.SystemPropertiesWrapper;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import javax.servlet.http.HttpServletRequest;
-
+@RunWith(MockitoJUnitRunner.class)
 public class PropertyControllerTest {
 
-	private PropertyController createTestSubject() {
-		return new PropertyController();
-	}
+    private static final String GET_PROPERTY = "/get_property/{name}/{defaultvalue}";
+    private static final String CATEGORY_PARAMETER = "/category_parameter";
 
-	@Test
-	public void testWelcome() throws Exception {
-		PropertyController testSubject;
-		HttpServletRequest request = null;
-		ModelAndView result;
+    private static final String ERROR_MSG = "Internal error occurred: ";
+    private static final String FAMILY_NAME = "familyName";
 
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.welcome(request);
-	}
+    private PropertyController propertyController;
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper;
 
+    @Mock
+    private CategoryParameterService service;
+    @Mock
+    private SystemPropertiesWrapper systemPropertiesWrapper;
 
-	@Test
-	public void testGetProperty() throws Exception {
-		PropertyController testSubject;
-		String name = "";
-		String defaultvalue = "";
-		HttpServletRequest request = null;
-		ResponseEntity<String> result;
+    @Before
+    public void setUp() {
+        propertyController = new PropertyController(service, systemPropertiesWrapper);
+        BasicConfigurator.configure();
+        mockMvc = MockMvcBuilders.standaloneSetup(propertyController).build();
+        objectMapper = new ObjectMapper();
+    }
 
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getProperty(name, defaultvalue, request);
-	}
+    @Test
+    public void shouldReturnInputJson_whenPropertyIsNotFound() throws Exception {
+        String inputJson = "{key1: val1}";
+        given(systemPropertiesWrapper.getOrDefault("name.1", inputJson)).willReturn(inputJson);
+
+        mockMvc.perform(get(GET_PROPERTY, "name_1", inputJson)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().json(inputJson));
+    }
+
+    @Test
+    public void shouldReturnGivenJson_whenPropertyIsFound() throws Exception {
+        String propertyJson = "{key1: val1}";
+        String inputJson = "{key2: val2}";
+        given(systemPropertiesWrapper.getOrDefault("name.1", inputJson)).willReturn(propertyJson);
+
+        mockMvc.perform(get(GET_PROPERTY, "name_1", inputJson)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().json(propertyJson));
+    }
+
+    @Test
+    public void shouldReturnInternalServerError_whenExceptionIsThrownFromSystemProperties() throws Exception {
+        String exceptionMessage = "Test exception message from system properties";
+        String inputJson = "{key1: val1}";
+        given(systemPropertiesWrapper.getOrDefault("name.1", inputJson)).willThrow(new RuntimeException(exceptionMessage));
+
+        mockMvc.perform(get(GET_PROPERTY, "name_1", inputJson)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isInternalServerError())
+            .andExpect(content().string(ERROR_MSG + exceptionMessage));
+    }
+
+    @Test
+    public void shouldReturnResponse_whenResponseIsFound() throws Exception {
+
+        CategoryParametersResponse categoryParametersResponse =
+            new CategoryParametersResponse(
+                ImmutableMap.of(
+                    "key1", ImmutableList.of(
+                        new CategoryParameterOptionRep("testId", "testName"))));
+
+        given(service.getCategoryParameters(Family.PARAMETER_STANDARDIZATION)).willReturn(categoryParametersResponse);
+
+        mockMvc.perform(get(CATEGORY_PARAMETER)
+            .param(FAMILY_NAME, "PARAMETER_STANDARDIZATION")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().json(objectMapper.writeValueAsString(categoryParametersResponse)));
+    }
+
+    @Test
+    public void shouldReturnInternalServerError_whenExceptionIsThrownFromService() throws Exception {
+        String exceptionMessage = "Test exception message from category parameter service";
+        given(service.getCategoryParameters(Family.PARAMETER_STANDARDIZATION)).willThrow(new RuntimeException(
+            exceptionMessage));
+
+        mockMvc.perform(get(CATEGORY_PARAMETER)
+            .param(FAMILY_NAME, "PARAMETER_STANDARDIZATION")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isInternalServerError())
+            .andExpect(content().string(ERROR_MSG + exceptionMessage));
+    }
 }
