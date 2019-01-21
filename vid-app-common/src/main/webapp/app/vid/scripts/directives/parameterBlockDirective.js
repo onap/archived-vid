eb1659273043b496ed93dc369db1c9311ac0ca49
Driptaroop Das <driptaroop.das@in.ibm.com>
2019-01-21 13:13:21 +0530

    bug fix - unreachable code
    bug fix - unreachable code.
    
    Issue-ID: VID-390
    Change-Id: I5e6cb45fed4cceb07d67ea22c011d329f1807678
    Signed-off-by: Driptaroop Das <driptaroop.das@in.ibm.com>

--- vid-app-common/src/main/webapp/app/vid/scripts/directives/parameterBlockDirective.js
+++ vid-app-common/src/main/webapp/app/vid/scripts/directives/parameterBlockDirective.js
@@ -3,6 +3,7 @@
  * VID
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2019 IBM.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -164,7 +165,7 @@ var parameterBlockDirective = function($log, PARAMETER, UtilityService, $compile
                 if (parameter.value) {
                     return "<select" + attributeString + " style='" + selectStyle
                         + additionalStyle + "'>" + "<option value=true>true</option>"
-                        + "<option value=false>false</option>";
+                        + "<option value=false>false</option>"
                     + "</select>";
                 }else{
                     return "<select" + attributeString + " style='" + selectStyle
