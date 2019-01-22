0f1a5582c110480534d80fffd53717cc08b8ad2f
Wojciech Sliwka <wojciech.sliwka@nokia.com>
2019-01-22 14:49:40 +0100

    Replaced tests for roles Issue-ID: VID-388
    Change-Id: I7c8c4ec50312f7ccd3a40eb2568cb8181f50b5d8
    Signed-off-by: Wojciech Sliwka <wojciech.sliwka@nokia.com>

--- vid-app-common/src/test/java/org/onap/vid/roles/RoleValidatorTest.java
+++ vid-app-common/src/test/java/org/onap/vid/roles/RoleValidatorTest.java
@@ -1,59 +1,106 @@
 package org.onap.vid.roles;
 
-import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+import org.assertj.core.util.Lists;
+import org.testng.annotations.Test;
 import org.onap.vid.mso.rest.RequestDetails;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class RoleValidatorTest {
 
-    private RoleValidator createTestSubject() {
-        return new RoleValidator(null);
+    private static final Role SAMPLE_ROLE = new Role(EcompRole.READ, "sampleSubscriber", "sampleServiceType", "sampleTenant");
+    private static final String SAMPLE_SUBSCRIBER = "sampleSubscriber";
+    private static final String NOT_MATCHING_SUBSCRIBER = "notMatchingSubscriber";
+    private static final String SAMPLE_SERVICE_TYPE = "sampleServiceType";
+    private static final String NOT_MATCHING_TENANT = "notMatchingTenant";
+    private static final String SAMPLE_TENANT = "sampleTenant";
+
+    private List<Role> roles = Lists.list(SAMPLE_ROLE);
+    private Map<String, Object> subscriberInfo = ImmutableMap.of("globalSubscriberId", "sampleSubscriber");
+    private Map<String, Object> requestParameters = ImmutableMap.of("subscriptionServiceType", "sampleServiceType");
+    private Map<String, Object> requestDetailsProperties = ImmutableMap.of("subscriberInfo", subscriberInfo, "requestParameters", requestParameters);
+    private RequestDetails requestDetails = new RequestDetails();
+
+    private RoleValidator roleValidator = new RoleValidator(roles);
+
+    @Test
+    public void shouldPermitSubscriberWhenNameMatchesAndRolesAreEnabled() {
+        roleValidator.enableRoles();
+
+        assertThat(roleValidator.isSubscriberPermitted(SAMPLE_SUBSCRIBER)).isTrue();
     }
 
     @Test
-    public void testIsMsoRequestValid() throws Exception {
-        RoleValidator testSubject;
-        RequestDetails mso_request = null;
-        boolean result;
+    public void shouldNotPermitSubscriberWhenNameNotMatches() {
+        roleValidator.enableRoles();
 
-        // default test
-        testSubject = createTestSubject();
-        result = testSubject.isMsoRequestValid(mso_request);
+        assertThat(roleValidator.isSubscriberPermitted(NOT_MATCHING_SUBSCRIBER)).isFalse();
     }
 
     @Test
-    public void testIsServicePermitted() throws Exception {
-        RoleValidator testSubject;
-        String subscriberName = "";
-        String serviceType = "";
-        boolean result;
+    public void shouldPermitServiceWhenNamesMatches() {
+        roleValidator.enableRoles();
 
-        // default test
-        testSubject = createTestSubject();
-        result = testSubject.isServicePermitted(subscriberName, serviceType);
+        assertThat(roleValidator.isServicePermitted(SAMPLE_SUBSCRIBER, SAMPLE_SERVICE_TYPE)).isTrue();
+    }
+
+
+    @Test
+    public void shouldNotPermitServiceWhenSubscriberNameNotMatches() {
+        roleValidator.enableRoles();
+
+        assertThat(roleValidator.isServicePermitted(NOT_MATCHING_SUBSCRIBER, SAMPLE_SERVICE_TYPE)).isFalse();
     }
 
     @Test
-    public void testIsSubscriberPermitted() throws Exception {
-        RoleValidator testSubject;
-        String subscriberName = "";
-        boolean result;
+    public void shouldNotPermitServiceWhenServiceTypeNotMatches() {
+        roleValidator.enableRoles();
 
-        // default test
-        testSubject = createTestSubject();
-        result = testSubject.isSubscriberPermitted(subscriberName);
+        assertThat(roleValidator.isServicePermitted(SAMPLE_SUBSCRIBER, NOT_MATCHING_SUBSCRIBER)).isFalse();
     }
 
     @Test
-    public void testIsTenantPermitted() throws Exception {
-        RoleValidator testSubject;
-        String globalCustomerId = "";
-        String serviceType = "";
-        String tenantName = "";
-        boolean result;
+    public void shouldPermitTenantWhenNameMatches() {
+        roleValidator.enableRoles();
 
-        // default test
-        testSubject = createTestSubject();
-        result = testSubject.isTenantPermitted(globalCustomerId, serviceType, tenantName);
+        assertThat(roleValidator.isTenantPermitted(SAMPLE_SUBSCRIBER, SAMPLE_SERVICE_TYPE, SAMPLE_TENANT)).isTrue();
     }
 
+
+    @Test
+    public void shouldNotPermitTenantWhenNameNotMatches() {
+        roleValidator.enableRoles();
+
+        assertThat(roleValidator.isTenantPermitted(SAMPLE_SUBSCRIBER, SAMPLE_SERVICE_TYPE, NOT_MATCHING_TENANT)).isFalse();
+    }
+
+    @Test
+    public void shouldValidateProperlySORequest() {
+        roleValidator.enableRoles();
+        requestDetails.setAdditionalProperty("requestDetails", requestDetailsProperties);
+
+        assertThat(roleValidator.isMsoRequestValid(requestDetails)).isTrue();
+    }
+
+    @Test
+    public void shouldValidateUnknownSORequest() {
+        roleValidator.enableRoles();
+
+        assertThat(roleValidator.isMsoRequestValid(new RequestDetails())).isTrue();
+    }
+
+    @Test
+    public void shouldRejectSORequestWhenSubscriberNotMatches() {
+        roleValidator.enableRoles();
+        Map<String, Object> subscriberInfo = ImmutableMap.of("globalSubscriberId", "sample");
+        Map<String, Object> requestDetailsProperties = ImmutableMap.of("subscriberInfo", subscriberInfo, "requestParameters", requestParameters);
+        requestDetails.setAdditionalProperty("requestDetails", requestDetailsProperties);
+
+        assertThat(roleValidator.isMsoRequestValid(requestDetails)).isFalse();
+    }
 }
