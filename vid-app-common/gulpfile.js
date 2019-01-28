1139e4f2e82661cc5a8edfb434e9aca97a37fa29
golabek <tomasz.golabek@nokia.com>
2019-01-28 09:34:48 +0100

    Introduce JS unit tests into VID
    maven -> npm -> gulp -> jest frameworks added in a mentioned order.
    Also added gulp step to collect external libraries (to confirm with Ittay).
    
    Change-Id: I1865228973eb31188fb052e8c9629f0ac01e48a7
    Issue-ID: VID-391
    Signed-off-by: Tomasz Golabek <tomasz.golabek@nokia.com>

---
+++ vid-app-common/gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+var runSequence = require('gulp-run-sequence');
+var dependencies = require('./package.json').dependencies;
+var filter = require('gulp-filter');
+var flatten = require('gulp-flatten');
+var jest = require('jest');
+
+// -------------- Find and copy external dependencies --------------
+
+const dest_path = './src/main/webapp/resources/vendor/';
+const node_modules = './node_modules/';
+
+var depList = () => {
+  var deps = [];
+  for (let dependenciesKey in dependencies) {
+    if (dependencies.hasOwnProperty(dependenciesKey)) {
+      deps.push(node_modules + dependenciesKey + "/**");
+    }
+  }
+  return deps;
+};
+
+gulp.task('copyJs', function () {
+  const jsFilter = filter('**/*.min.js', {passthrough: false});
+  return gulp.src(depList())
+  .pipe(jsFilter)
+  .pipe(flatten())
+  .pipe(gulp.dest(dest_path + '/js/'));
+});
+
+gulp.task('copyCss', function () {
+  const cssFilter = filter('**/*.min.css', {passthrough: false});
+  return gulp.src(depList())
+  .pipe(cssFilter)
+  .pipe(flatten())
+  .pipe(gulp.dest(dest_path + '/css/'));
+});
+
+gulp.task('copyFonts', function () {
+  const fontFilter = filter(['**/*.eot', "**/*.svg", "**/*.ttf", "**/*.woff", "**/*.woff2"], {passthrough: false});
+  return gulp.src(depList())
+  .pipe(fontFilter)
+  .pipe(flatten())
+  .pipe(gulp.dest(dest_path + '/fonts/'));
+});
+
+// -------------- Run Jest Tests --------------
+
+gulp.task('test', function() {
+  jest.runCLI({}, ".")
+  .then((result) => {
+    if (!result.results.success){
+      console.log("Execution of js tests failed with status 1");
+      process.exit(1);
+    }
+  });
+});
+
+// -------------- Default Task --------------
+
+gulp.task('default', function () {
+  return runSequence('copyCss', "copyFonts", "copyJs", "test");
+});
