ab4d74f9cece8bab3f0a4aab6a1db48ac5766892
golabek <tomasz.golabek@nokia.com>
2019-01-28 09:34:48 +0100

    Introduce JS unit tests into VID
    maven -> npm -> gulp -> jest frameworks added in a mentioned order.
    
    Change-Id: I1865228973eb31188fb052e8c9629f0ac01e48a7
    Issue-ID: VID-391
    Signed-off-by: Tomasz Golabek <tomasz.golabek@nokia.com>

---
+++ vid-app-common/src/main/webapp/app/vid/scripts/controller/aaiSubscriberController.test.js
@@ -0,0 +1,33 @@
+require('./aaiSubscriberController');
+const jestMock = require('jest-mock');
+
+describe('TreeCtrl testing', () => {
+  let $scope;
+  beforeEach(
+      angular.mock.module('app')
+  );
+
+  beforeEach(inject(function (_$controller_) {
+    $scope = {};
+    _$controller_('TreeCtrl', {
+      $scope: $scope
+    });
+  }));
+
+  test('Verify expandAll calls broadcast with expand-all parameter', () => {
+    // given
+    const broadcast = jestMock.fn();
+    $scope.$broadcast = broadcast;
+    FIELD = {
+      ID: {
+        ANGULAR_UI_TREE_EXPANDALL: "angular-ui-tree:expand-all"
+      }
+    };
+    // when
+    $scope.expandAll();
+    // then
+    expect(broadcast).toHaveBeenCalledWith("angular-ui-tree:expand-all");
+  });
+
+});
+
