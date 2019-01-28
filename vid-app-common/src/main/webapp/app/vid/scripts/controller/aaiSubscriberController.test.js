bc7dee37e8cd9cd63b8ebec53173b8e7da8673af
golabek <tomasz.golabek@nokia.com>
2019-01-28 09:34:48 +0100

    Introduce JS unit tests into VID
    maven -> npm -> gulp -> jest frameworks added in a mentioned order.
    Also added gulp step to collect external libraries (to confirm with Ittay).
    
    Change-Id: I1865228973eb31188fb052e8c9629f0ac01e48a7
    Issue-ID: VID-391
    Signed-off-by: Tomasz Golabek <tomasz.golabek@nokia.com>

---
+++ vid-app-common/src/main/webapp/app/vid/scripts/controller/aaiSubscriberController.test.js
@@ -0,0 +1,26 @@
+require('./aaiSubscriberController');
+
+describe('TreeCtrl testing', () => {
+  beforeEach(
+    angular.mock.module('app')
+  );
+
+  var $scope;
+
+  beforeEach(angular.mock.inject(function(_$controller_){
+    $scope={};
+    _$controller_('TreeCtrl', {
+      $scope: $scope,
+    });
+  }));
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect($scope.sum(1, 2)).toBe(3);
+  });
+
+  test('adds 1 + 2 to equal 4', () => {
+    expect($scope.sum(1, 2)).toBe(4);
+  });
+
+});
+
