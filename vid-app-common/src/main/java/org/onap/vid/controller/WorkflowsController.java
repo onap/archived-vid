653985bcf8b0d1a159b7a74dc263ac34f6c40666
golabek <tomasz.golabek@nokia.com>
2019-02-07 12:21:57 +0100

    Mocked BE service to return workflow list
    Mocked SO workflows endpoint and internal layer of service introduced to handle it
    
    Change-Id: I28c2b8e70d22fe694d2d096ed041272e6cda958c
    Issue-ID: VID-400
    Signed-off-by: Tomasz Golabek <tomasz.golabek@nokia.com>

---
+++ vid-app-common/src/main/java/org/onap/vid/controller/WorkflowsController.java
@@ -0,0 +1,31 @@
+package org.onap.vid.controller;
+
+import java.util.List;
+import org.onap.portalsdk.core.controller.RestrictedBaseController;
+import org.onap.vid.model.SOWorkflow;
+import org.onap.vid.services.ExtWorkflowsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(WorkflowsController.WORKFLOWS_MANAGEMENT)
+public class WorkflowsController extends RestrictedBaseController {
+    static final String WORKFLOWS_MANAGEMENT = "workflows-management";
+
+    private ExtWorkflowsService extWorkflowsService;
+
+    @Autowired
+    public WorkflowsController(ExtWorkflowsService extWorkflowsService) {
+        this.extWorkflowsService = extWorkflowsService;
+    }
+
+    @RequestMapping(value = "workflows", method = RequestMethod.GET)
+    public List<SOWorkflow> getWorkflows(@RequestParam(value = "vnfName") String vnfName){
+        return extWorkflowsService.getWorkflows(vnfName);
+    }
+
+}
+
