c79087597e1861d12b691d1db4011641a78ab3c1
Wojciech Sliwka <wojciech.sliwka@nokia.com>
2019-02-13 10:38:09 +0100

    workflow parameters API
    Change-Id: If79e410608a0562ddef86d9a3a451d4addf66a0f
    Issue-ID: VID-419
    Signed-off-by: Wojciech Sliwka <wojciech.sliwka@nokia.com>

--- vid-app-common/src/main/java/org/onap/vid/services/ExtWorkflowsService.java
+++ vid-app-common/src/main/java/org/onap/vid/services/ExtWorkflowsService.java
@@ -2,7 +2,10 @@ package org.onap.vid.services;
 
 import java.util.List;
 import org.onap.vid.model.SOWorkflow;
+import org.onap.vid.model.SOWorkflowParameterDefinitions;
 
 public interface ExtWorkflowsService {
     List<SOWorkflow> getWorkflows(String vnfName);
+
+    SOWorkflowParameterDefinitions getWorkflowParameterDefinitions(Long workflowId);
 }
