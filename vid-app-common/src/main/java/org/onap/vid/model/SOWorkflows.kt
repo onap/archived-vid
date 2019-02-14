3c6a1844083f02796e67fd185306a8d0cdd7da07
golabek <tomasz.golabek@nokia.com>
2019-02-14 15:13:04 +0100

    Introduced dynamic workflow properties in VID FE
    VID FE will get workflows from SO API (mocked at this moment),then call SO for the parameters and dynamically render those parameters on the
    page.
    (contains PR of @Wojciech Sliwka about mocked parameters API from SO)
    
    Change-Id: If2cd156cd96a120e79746a4da44d4926f8d6cbcf
    Issue-ID: VID-398
    Signed-off-by: Tomasz Golabek <tomasz.golabek@nokia.com>

--- vid-app-common/src/main/java/org/onap/vid/model/SOWorkflows.kt
+++ vid-app-common/src/main/java/org/onap/vid/model/SOWorkflows.kt
@@ -1,21 +1,26 @@
 package org.onap.vid.model
 
-data class SOWorkflow @JvmOverloads constructor(var id: Long? = null, var name: String? = null) {
+data class SOWorkflow constructor(val id: Long, val name: String) {
     fun clone(): SOWorkflow {
         return copy()
     }
 }
 
-data class SOWorkflows @JvmOverloads constructor(var workflows: List<SOWorkflow>? = emptyList()) {
+data class SOWorkflows @JvmOverloads constructor(val workflows: List<SOWorkflow> = emptyList()) {
     fun clone(): SOWorkflows {
-        return copy(workflows?.toMutableList())
+        return copy(workflows.toMutableList())
     }
 }
 
-
-enum class SOWorkflowType(var type: String? = "STRING") {
+enum class SOWorkflowType(val type: String) {
     STRING("STRING")
 }
 
-data class SOWorkflowParameterDefinition(var id: Long? = null, var name: String? = null, var pattern: String? = null, var type: SOWorkflowType? = null, val required: Boolean? = null)
-data class SOWorkflowParameterDefinitions(var parameterDefinitions: List<SOWorkflowParameterDefinition>? = null)
+data class SOWorkflowParameterDefinition constructor(val id: Long, val name: String, val pattern: String,
+                                         val type: SOWorkflowType, val required: Boolean)
+
+data class SOWorkflowParameterDefinitions constructor(val parameterDefinitions: List<SOWorkflowParameterDefinition> = emptyList()) {
+    fun clone(): SOWorkflowParameterDefinitions {
+        return copy(parameterDefinitions.toMutableList())
+    }
+}
