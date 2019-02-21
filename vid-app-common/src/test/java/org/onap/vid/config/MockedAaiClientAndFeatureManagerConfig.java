5943337e7b242a52e1d50feae43332344ebf536d
Ittay Stern <ittay.stern@att.com>
2019-02-21 14:04:39 +0200

    Enable AsyncInstantiationBusinessLogic tests
    Issue-ID: VID-378
    Change-Id: If0dd6147f1ce7fba3db01c85359f17abad8bcb07
    Signed-off-by: Ittay Stern <ittay.stern@att.com>

--- vid-app-common/src/test/java/org/onap/vid/config/MockedAaiClientAndFeatureManagerConfig.java
+++ vid-app-common/src/test/java/org/onap/vid/config/MockedAaiClientAndFeatureManagerConfig.java
@@ -2,6 +2,8 @@ package org.onap.vid.config;
 
 import org.mockito.Mockito;
 import org.onap.vid.aai.AaiClientInterface;
+import org.onap.vid.services.CloudOwnerService;
+import org.onap.vid.services.CloudOwnerServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.togglz.core.manager.FeatureManager;
@@ -12,6 +14,11 @@ public class MockedAaiClientAndFeatureManagerConfig {
     @Bean
     public FeatureManager featureManager() {
         return Mockito.mock(FeatureManager.class);
+    }
+
+    @Bean
+    public CloudOwnerService cloudOwnerService(AaiClientInterface aaiClient, FeatureManager featureManager) {
+        return new CloudOwnerServiceImpl(aaiClient, featureManager);
     }
 
     @Bean
