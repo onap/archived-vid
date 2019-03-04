65bff5731d5a35629825ccc565862ec1d847847f
Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
2019-03-04 15:55:31 +0100

    Increasing test coverage for vid.mso.model
    Change-Id: Id9e5aefddbb47a60bb937c89a25d5584505e2edb
    Issue-ID: VID-387
    Signed-off-by: Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>

--- vid-app-common/src/test/java/org/onap/vid/mso/model/OperationalEnvironmentActivateInfoTest.java
+++ vid-app-common/src/test/java/org/onap/vid/mso/model/OperationalEnvironmentActivateInfoTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,45 +20,61 @@
 
 package org.onap.vid.mso.model;
 
-import org.junit.Test;
+import org.mockito.Mock;
 import org.onap.vid.controller.OperationalEnvironmentController;
-import org.onap.vid.controller.OperationalEnvironmentController.OperationalEnvironmentActivateBody;
 import org.onap.vid.controller.OperationalEnvironmentController.OperationalEnvironmentManifest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class OperationalEnvironmentActivateInfoTest {
 
-    private OperationalEnvironmentActivateInfo createTestSubject() {
-        OperationalEnvironmentController.OperationalEnvironmentActivateBody a = new OperationalEnvironmentActivateBody("a", "b", "c", new OperationalEnvironmentManifest());
-        return new OperationalEnvironmentActivateInfo(a, "", "");
+    @Mock
+    private OperationalEnvironmentController.OperationalEnvironmentActivateBody operationalEnvironmentActivateBody;
+
+    @BeforeClass
+    public void setUp() {
+        initMocks(this);
     }
 
     @Test
-    public void testGetUserId() throws Exception {
-        OperationalEnvironmentActivateInfo testSubject;
-        String result;
+    public void shouldProperlyCreateOperationalEnvironmentActivateInfoWithParameters() {
+        //  given
+        String relatedInstanceId = "testRelatedInstanceId";
+        String relatedInstanceName = "testRelatedInstanceName";
+        String workloadContext = "testWorkloadContext";
+        String userId = "testUserId";
+        String operationalEnvironmentId = "testOperationalEnvironmentId";
+        OperationalEnvironmentManifest manifest = new OperationalEnvironmentManifest();
 
-        // default test
-        testSubject = createTestSubject();
-        result = testSubject.getUserId();
+        when(operationalEnvironmentActivateBody.getRelatedInstanceId()).thenReturn(relatedInstanceId);
+        when(operationalEnvironmentActivateBody.getRelatedInstanceName()).thenReturn(relatedInstanceName);
+        when(operationalEnvironmentActivateBody.getWorkloadContext()).thenReturn(workloadContext);
+        when(operationalEnvironmentActivateBody.getManifest()).thenReturn(manifest);
+
+
+        //  when
+        OperationalEnvironmentActivateInfo operationalEnvironmentActivateInfo =
+                new OperationalEnvironmentActivateInfo(operationalEnvironmentActivateBody, userId, operationalEnvironmentId);
+
+
+        //  then
+        assertThat(operationalEnvironmentActivateInfo.getUserId()).isEqualTo(userId);
+        assertThat(operationalEnvironmentActivateInfo.getOperationalEnvironmentId()).isEqualTo(operationalEnvironmentId);
+
+        assertThat(operationalEnvironmentActivateInfo.toString()).isEqualToIgnoringWhitespace(
+                "OperationalEnvironmentActivateInfo{operationalEnvironmentId="+operationalEnvironmentId+"," +
+                        " userId="+userId+"," +
+                        " super=OperationalEnvironmentActivateInfo{" +
+                        " relatedInstanceId="+relatedInstanceId+"," +
+                        " relatedInstanceName="+relatedInstanceName+"," +
+                        " workloadContext="+workloadContext+"," +
+                        " manifest="+manifest.toString()+"}}"
+        );
+
     }
 
-    @Test
-    public void testGetOperationalEnvironmentId() throws Exception {
-        OperationalEnvironmentActivateInfo testSubject;
-        String result;
-
-        // default test
-        testSubject = createTestSubject();
-        result = testSubject.getOperationalEnvironmentId();
-    }
-
-    @Test
-    public void testToString() throws Exception {
-        OperationalEnvironmentActivateInfo testSubject;
-        String result;
-
-        // default test
-        testSubject = createTestSubject();
-        result = testSubject.toString();
-    }
 }
