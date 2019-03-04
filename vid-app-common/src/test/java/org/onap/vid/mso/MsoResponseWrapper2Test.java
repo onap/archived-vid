448b65dde75f4c31138dc32ff570a05946ff43e3
Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
2019-03-04 16:01:39 +0100

    Increasing test coverage for vid.mso
    Change-Id: I3b9c4aeb628a50e5bb9032dd78605cfa0a4c3f57
    Issue-ID: VID-387
    Signed-off-by: Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>

--- vid-app-common/src/test/java/org/onap/vid/mso/MsoResponseWrapper2Test.java
+++ vid-app-common/src/test/java/org/onap/vid/mso/MsoResponseWrapper2Test.java
@@ -3,13 +3,14 @@
  * VID
  * ================================================================================
  * Copyright (C) 2017 - 2019 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2019 Nokia. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,41 +21,75 @@
 
 package org.onap.vid.mso;
 
-import org.junit.Test;
+import static org.mockito.Mockito.when;
+
+import io.joshworks.restclient.http.HttpResponse;
+import org.mockito.Mock;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class MsoResponseWrapper2Test {
 
-    private MsoResponseWrapper2 createTestSubject() {
-        return new MsoResponseWrapper2(new RestObject());
+    @Mock
+    private RestObject<String> msoResponse;
+
+    @Mock
+    private HttpResponse<String> httpResponse;
+
+    private MsoResponseWrapper2<String> responseWrapper;
+
+    private int status = 202;
+    private String entity = "testEntity";
+    private String rawString = "testRawString";
+
+    @BeforeClass
+    public void setUp() {
+        initMocks(this);
     }
 
     @Test
-    public void testGetStatus() throws Exception {
-        MsoResponseWrapper2 testSubject;
-        int result;
+    public void shouldProperlyCreateInstanceFromRestObject() {
+        //  given
+        when(msoResponse.getStatusCode()).thenReturn(status);
+        when(msoResponse.get()).thenReturn(entity);
+        when(msoResponse.getRaw()).thenReturn(rawString);
 
-        // default test
-        testSubject = createTestSubject();
-        result = testSubject.getStatus();
+        //  when
+        responseWrapper = new MsoResponseWrapper2<>(msoResponse);
+
+        //  then
+        assertThat(responseWrapper.getStatus()).isEqualTo(status);
+        assertThat(responseWrapper.getEntity()).isEqualTo(entity);
+        assertThat(responseWrapper.getResponse()).isEqualTo("{\"status\":"+status+",\"entity\":\""+entity+"\"}");
     }
 
     @Test
-    public void testGetResponse() throws Exception {
-        MsoResponseWrapper2 testSubject;
-        String result;
+    public void shouldProperlyCreateInstanceFromHttpResponse() {
+        //  given
+        when(httpResponse.getStatus()).thenReturn(status);
+        when(httpResponse.getBody()).thenReturn(entity);
 
-        // default test
-        testSubject = createTestSubject();
-        result = testSubject.getResponse();
+        //  when
+        responseWrapper = new MsoResponseWrapper2<>(httpResponse);
+
+        //  then
+        assertThat(responseWrapper.getStatus()).isEqualTo(status);
+        assertThat(responseWrapper.getEntity()).isEqualTo(entity);
+        assertThat(responseWrapper.getResponse()).isEqualTo("{\"status\":"+status+",\"entity\":\""+entity+"\"}");
     }
 
     @Test
-    public void testGetEntity() throws Exception {
-        MsoResponseWrapper2 testSubject;
-        Object result;
+    public void shouldProperlyCreateInstanceFromStatusAndEntity() {
+        //  when
+        responseWrapper = new MsoResponseWrapper2<>(status,entity);
 
-        // default test
-        testSubject = createTestSubject();
-        result = testSubject.getEntity();
+        //  then
+        assertThat(responseWrapper.getStatus()).isEqualTo(status);
+        assertThat(responseWrapper.getEntity()).isEqualTo(entity);
+        assertThat(responseWrapper.getResponse()).isEqualTo("{\"status\":"+status+",\"entity\":\""+entity+"\"}");
     }
+
 }
