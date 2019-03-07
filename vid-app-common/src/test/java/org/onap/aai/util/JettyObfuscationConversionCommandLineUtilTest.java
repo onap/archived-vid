72232bf7fdc36b7bbab44dbf5400df48c36bafde
Ittay Stern <ittay.stern@att.com>
2019-03-07 11:47:30 +0200

    Fix JettyObfuscationConversionCommandLineUtilTest
    Change-Id: Ib2e4559925816fad5ad39985d1abda298cd3d165
    Issue-ID: VID-431
    Signed-off-by: Ittay Stern <ittay.stern@att.com>

--- vid-app-common/src/test/java/org/onap/aai/util/JettyObfuscationConversionCommandLineUtilTest.java
+++ vid-app-common/src/test/java/org/onap/aai/util/JettyObfuscationConversionCommandLineUtilTest.java
@@ -20,23 +20,51 @@
 
 package org.onap.aai.util;
 
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.io.PrintStream;
+import java.util.Arrays;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.onap.vid.aai.util.JettyObfuscationConversionCommandLineUtil;
 
+@RunWith(Parameterized.class)
 public class JettyObfuscationConversionCommandLineUtilTest {
 
-	private JettyObfuscationConversionCommandLineUtil createTestSubject() {
-		return new JettyObfuscationConversionCommandLineUtil();
+	final String[] args;
+	final String expected;
+
+	public JettyObfuscationConversionCommandLineUtilTest(String description, String[] args, String expected) {
+		this.args = args;
+		this.expected = expected;
 	}
 
-	
+	@Parameters(name = "{0}")
+	public static Iterable<Object[]> data() {
+		return Arrays.asList(new Object[][]{
+			{"print usage on missing params", new String[]{}, "usage:"},
+			{"obfuscate", new String[]{"-e", "foobar"}, "OBF:1vub1ua51uh81ugi1u9d1vuz"},
+			{"deobfuscate", new String[]{"-d", "OBF:1vub1ua51uh81ugi1u9d1vuz"}, "foobar"},
+			{"input parse exception", new String[]{"mm", "-mm", "-mm"}, "failed to parse input"},
+			{"deobfuscate exception", new String[]{"-d", "problematic string"}, "exception:java.lang.NumberFormatException"},
+		});
+	}
+
 	@Test
-	public void testMain() throws Exception {
-		String[] args = new String[] { "" };
-
-		// default test
-		JettyObfuscationConversionCommandLineUtil.main(args);
+	public void testMain() {
+		final PrintStream originalOut = System.out;
+		try {
+			PrintStream mockedOut = mock(PrintStream.class);
+			System.setOut(mockedOut);
+			JettyObfuscationConversionCommandLineUtil.main(args);
+			verify(mockedOut).println(contains(expected));
+		} finally {
+			System.setOut(originalOut);
+		}
 	}
 
-	
 }
