59b306a968fb7a2c4b10230018872d407a0447bc
Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
2019-06-12 15:55:45 +0200

    Adapting GUI for error report managing
    Change-Id: Idb3f5750596334990b030bfdf2f6551ad4d1c8e3
    Issue-ID: VID-488
    Signed-off-by: Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>

--- vid-app-common/src/main/webapp/app/vid/scripts/modals/new-change-management/new-change-management.controller.js
+++ vid-app-common/src/main/webapp/app/vid/scripts/modals/new-change-management/new-change-management.controller.js
@@ -26,8 +26,10 @@
 
     function newChangeManagementModalController($uibModalInstance, $uibModal,$q, AaiService, changeManagementService, Upload, $log, $scope, _, COMPONENT, VIDCONFIGURATION, DataService, featureFlags) {
 
+
         var vm = this;
         vm.hasScheduler = !!VIDCONFIGURATION.SCHEDULER_PORTAL_URL;
+        vm.errorMsg='';
 
         vm.wizardStep = 1;
         vm.nextStep = function(){
@@ -43,6 +45,24 @@
 
         var attuid;
 
+        $scope.showReportWindow = function() {
+            const modalWindow = $uibModal.open({
+                templateUrl: 'app/vid/scripts/modals/report-modal/report-modal.html',
+                controller: 'reportModalController',
+                controllerAs: 'vm',
+                resolve: {
+                    errorMsg: function () {
+                        return vm.errorMsg.message;
+                    }
+                }
+            });
+
+        };
+
+        $scope.isShowErrorReport = function() {
+            return featureFlags.isOn(COMPONENT.FEATURE_FLAGS.FLAG_CREATE_ERROR_REPORTS);
+        };
+
         function fetchAttUid() {
             var defer = $q.defer();
             if (attuid) {
@@ -54,6 +74,7 @@
                     },
                     function (err) {
                         defer.reject(err);
+                        vm.errorMsg = err;
                     });
             }
             return defer.promise;
@@ -75,6 +96,7 @@
                 })
                 .catch(function (error) {
                     $log.error(error);
+                    vm.errorMsg = err;
                 })
         };
 
@@ -132,6 +154,7 @@
                                     newVNFName.availableVersions = _.sortBy(_.uniq(versions, response.data.service, true),"modelInfo.modelVersion");
                                 }).catch(function (error) {
                                 $log.error(error);
+                                vm.errorMsg = error;
                             });
                         });
                     }
@@ -374,6 +397,7 @@
                 }
 				}catch(err){
 					$log.error('SchedulerCtrl::extractChangeManagementCallbackDataStr error: ' + err);
+					vm.errorMsg = err;
 				}
 
 				result.requestDetails.push(data);
@@ -477,6 +501,7 @@
                 })
                 .catch(function (error) {
                     $log.error(error);
+                    vm.errorMsg = error;
                 });
         };
 
@@ -604,6 +629,7 @@
                     })
                     .catch(function (error) {
                         reject(error);
+                        vm.errorMsg = error;
                     });
             });
         }
@@ -710,6 +736,7 @@
             vm.localWorkflows = response.data.workflows || [];
           }).catch(function (error) {
             $log.error(error);
+            vm.errorMsg = error;
           });
         };
 
@@ -720,6 +747,7 @@
             vm.remoteWorkflows = response.data || [];
           }).catch(function (error) {
             $log.error(error);
+            vm.errorMsg = error;
           });
         };
 
@@ -736,6 +764,7 @@
           })
           .catch(function (error) {
             $log.error(error);
+            vm.errorMsg = error;
           });
         };
 
@@ -828,6 +857,7 @@
                         vnfName[0].selectedFile = JSON.parse(lines);
                     } catch (error) {
                         $log.error(error);
+                        vm.errorMsg = error;
                     }
                 };
                 fileReader.readAsText(file);
