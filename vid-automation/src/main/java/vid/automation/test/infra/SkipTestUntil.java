1c9c922672e53a16d4fccb281c21cc7e2818f60b
Eylon Malin <eylon.malin@intl.att.com>
2019-08-15 11:39:22 +0300

    Skip log format validation for a week
    Add annotation SkipTestUntil and TestNg transformer so we can add
    annotation for skip test till some date (aka time bomb)
    Use it in log format tests
    Issue-ID: VID-378
    
    Signed-off-by: Eylon Malin <eylon.malin@intl.att.com>
    Change-Id: Ib4b5d17d73d360fd2554b5ffc0373790d22d9e4e
    Signed-off-by: Eylon Malin <eylon.malin@intl.att.com>

---
+++ vid-automation/src/main/java/vid/automation/test/infra/SkipTestUntil.java
@@ -0,0 +1,21 @@
+package vid.automation.test.infra;
+
+import static java.lang.annotation.ElementType.METHOD;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/*
+Skip test until date (AKA TimeBomb)
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({METHOD})
+public @interface SkipTestUntil {
+
+    /**
+     * Date in the form of "2007-12-25"
+     */
+    String value();
+
+}
