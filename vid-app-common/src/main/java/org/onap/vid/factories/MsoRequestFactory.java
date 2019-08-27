cbb75bdf15f149c00f88ae03484e2736028d07ff
Thugutla sailakshmi <tsaila10@in.ibm.com>
2019-08-27 16:46:22 +0530

    Remove this unused method parameter "path".
    Remove this unused method parameter "path"
    
    Issue-ID: VID-561
    Change-Id: I85f63ed4bdcbc07b8469b3270c4ab68c28ee0fa9
    Signed-off-by: Thugutla sailakshmi <tsaila10@in.ibm.com>

--- vid-app-common/src/main/java/org/onap/vid/factories/MsoRequestFactory.java
+++ vid-app-common/src/main/java/org/onap/vid/factories/MsoRequestFactory.java
@@ -3,6 +3,7 @@
  * VID
  * ================================================================================
  * Copyright (C) 2017 - 2019 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2019 IBM.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +28,7 @@ import org.onap.vid.mso.rest.RequestDetails;
  */
 public class MsoRequestFactory {
 
-    public RequestDetails createMsoRequest(String path)
+    public RequestDetails createMsoRequest()
     {
         return null;
     }
