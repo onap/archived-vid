99d30bc03429e550f9b5f755e2361fa95d7663f2
Thugutla sailakshmi <tsaila10@in.ibm.com>
2019-08-28 20:57:33 +0530

    remove the commented block of code
    remove the commented block of code
    
    Issue-ID: VID-561
    Change-Id: I0381e0df62f685d54807896f36edc5efa5e0c193
    Signed-off-by: Thugutla sailakshmi <tsaila10@in.ibm.com>

--- vid-app-common/src/main/java/org/onap/vid/aai/model/AaiGetNetworkCollectionDetails/CloudRegion.java
+++ vid-app-common/src/main/java/org/onap/vid/aai/model/AaiGetNetworkCollectionDetails/CloudRegion.java
@@ -3,6 +3,7 @@
  * VID
  * ================================================================================
  * Copyright (C) 2017 - 2019 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2019 IBM.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,17 +51,6 @@ public class CloudRegion {
         return cloudRegionId;
     }
 
-    /*
-    This will handle container like:
-        {
-          "cloud-region": [{
-              "cloud-owner": "alfi",
-              "cloud-region-id": "foo",
-              . . .
-          }, {
-              "cloud-owner": "alba",
-              "cloud-region-id": "bar",
-     */
     public static class Collection {
         private final List<CloudRegion> cloudRegions;
 
