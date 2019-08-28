e3706941999495f7979d25d0f7f9d4a878aa39fd
Thugutla sailakshmi <tsaila10@in.ibm.com>
2019-08-28 20:38:38 +0530

    Annotate the "AaiModelWithRelationships" interface with the @FunctionalInterface annotation
    use Annotate
    
    Issue-ID: VID-561
    Change-Id: Icba60469d90c581f390a0a48d70edc8be85af16c
    Signed-off-by: Thugutla sailakshmi <tsaila10@in.ibm.com>

--- vid-app-common/src/main/java/org/onap/vid/aai/model/interfaces/AaiModelWithRelationships.java
+++ vid-app-common/src/main/java/org/onap/vid/aai/model/interfaces/AaiModelWithRelationships.java
@@ -3,6 +3,7 @@
  * VID
  * ================================================================================
  * Copyright (C) 2017 - 2019 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2019 IBM.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +23,7 @@ package org.onap.vid.aai.model.interfaces;
 
 import org.onap.vid.aai.model.AaiGetNetworkCollectionDetails.RelationshipList;
 
+@FunctionalInterface
 public interface AaiModelWithRelationships {
     RelationshipList getRelationshipList();
 }
