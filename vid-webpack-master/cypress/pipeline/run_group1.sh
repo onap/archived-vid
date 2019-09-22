4c6e7740d1d55e15c76bca33bf82f176bc4fac15
Ittay Stern <ittay.stern@att.com>
2019-09-22 13:41:36 +0300

    Add memory to Cypress
    Fixing error: FATAL ERROR: CALL_AND_RETRY_LAST Allocation failed -
    JavaScript heap out of memory
    
    Change-Id: Ifa12f3c39bdf29c3211a3c75bcc17876a2af5b85
    Issue-ID: VID-647
    Signed-off-by: Ittay Stern <ittay.stern@att.com>

--- vid-webpack-master/cypress/pipeline/run_group1.sh
+++ vid-webpack-master/cypress/pipeline/run_group1.sh
@@ -10,4 +10,4 @@ TESTS_GROUP_FILE=cypress/pipeline/group1.txt
 cd ${CYPRESS_HOME_DIR}
 
 cat ${TESTS_GROUP_FILE}
-npm run cypress:headless -- --spec=$(cat ${TESTS_GROUP_FILE} | tr '\n' ',')
+npm run cypress:headless --max-old-space-size=4096 -- --spec=$(cat ${TESTS_GROUP_FILE} | tr '\n' ',')
