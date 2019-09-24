c0f41858992090eff2e4ccb06d5e3332d8c008e0
Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
2019-09-24 07:54:09 +0200

    adding orchestration type to service models view
    Issue-ID: VID-602
    Signed-off-by: Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
    Change-Id: I289b55129cbfe8fbf6dd28f0e8f41715ff4ed107

--- vid-automation/src/main/java/vid/automation/test/test/BrowseASDCTest.java
+++ vid-automation/src/main/java/vid/automation/test/test/BrowseASDCTest.java
@@ -31,8 +31,9 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
-import static vid.automation.test.infra.Features.FLAG_1908_TRANSPORT_SERVICE_NEW_INSTANTIATION_UI;
 import static vid.automation.test.infra.Features.FLAG_5G_IN_NEW_INSTANTIATION_UI;
+import static vid.automation.test.infra.Features.FLAG_SHOW_ORCHESTRATION_TYPE;
+import static vid.automation.test.infra.Features.FLAG_1908_TRANSPORT_SERVICE_NEW_INSTANTIATION_UI;
 import static vid.automation.test.infra.ModelInfo.*;
 
 
@@ -478,5 +479,31 @@ public class BrowseASDCTest extends CreateInstanceDialogBaseTest {
         WebElement serviceModelsTbody = Get.byXpath("//table[@data-tests-id='serviceModelsTable']/tbody");
         assertFalse(Exists.tagNameInAnotherElement(serviceModelsTbody, "tr"), "Table should be empty on empty results");
         resetGetServicesCache();
+    }
+
+    @Test
+    @FeatureTogglingTest(FLAG_SHOW_ORCHESTRATION_TYPE)
+    public void browseSdcModel_filterModelsWithOrchestrationType_alacarte() {
+        resetGetServicesCache();
+        SimulatorApi.clearAll();
+        BrowseASDCPage browseAsdcPage = registerSimulatorAndGoToBrowseSDC();
+        GeneralUIUtils.ultimateWait();
+        assertThat(browseAsdcPage.countCurrentRowsInTable(),(Matchers.greaterThan(3)));
+        browseAsdcPage.fillFilterText("a la carte");
+        Assert.assertEquals(browseAsdcPage.countCurrentRowsInTable(),3);
+        browseAsdcPage.fillFilterText("");
+    }
+
+    @Test
+    @FeatureTogglingTest(FLAG_SHOW_ORCHESTRATION_TYPE)
+    public void browseSdcModel_filterModelsWithOrchestrationType_macro() {
+        resetGetServicesCache();
+        SimulatorApi.clearAll();
+        BrowseASDCPage browseAsdcPage = registerSimulatorAndGoToBrowseSDC();
+        GeneralUIUtils.ultimateWait();
+        assertThat(browseAsdcPage.countCurrentRowsInTable(),(Matchers.greaterThan(5)));
+        browseAsdcPage.fillFilterText("macro");
+        Assert.assertEquals(browseAsdcPage.countCurrentRowsInTable(),4);
+        browseAsdcPage.fillFilterText("");
     }
 }
