354a70c6e2c48bbe1103ad8c2d7599a16fba8ea6
Einat Vinouze <einat.vinouze@intl.att.com>
2019-09-25 16:06:48 +0300

    sort base resource according it's position field
    Issue-ID: VID-646
    Signed-off-by: Einat Vinouze <einat.vinouze@intl.att.com>
    Change-Id: I35705cd4a239e30bc6d86da7cde05495648d2a04

--- vid-app-common/src/test/java/org/onap/vid/job/command/ServiceInProgressStatusCommandTest.java
+++ vid-app-common/src/test/java/org/onap/vid/job/command/ServiceInProgressStatusCommandTest.java
@@ -143,9 +143,12 @@ public class ServiceInProgressStatusCommandTest {
         UUID uuid = UUID.randomUUID();
         String userId = "mockedUserID";
         String testApi = "VNF_API";
-        Vnf vnf1 = createVnf(emptyList(), Create);
-        Vnf vnf2 = createVnf(emptyList(), Create);
         Network network = createNetwork(Create);
+        network.setPosition(0);
+        Vnf vnf1 = createVnf(emptyList(), Create);
+        vnf1.setPosition(1);
+        Vnf vnf2 = createVnf(emptyList(), Create);
+        vnf2.setPosition(2);
         ServiceInstantiation serviceInstantiation = createService(
                 ImmutableList.of(vnf1, vnf2),
                 ImmutableList.of(network),
