fcc2fd537fd3d9b0880621187fdfaa1b8a817932
Ittay Stern <ittay.stern@att.com>
2019-10-06 18:58:19 +0300

    Verify partner-name header is sent to AAI in API-tests
    Issue-ID: VID-253
    
    Change-Id: Ic804b4dc1de2c492da02e47e86f0660b9a769ead
    Signed-off-by: Ittay Stern <ittay.stern@att.com>

--- vid-automation/src/main/java/org/onap/simulator/presetGenerator/presets/BasePresets/BaseAAIPreset.java
+++ vid-automation/src/main/java/org/onap/simulator/presetGenerator/presets/BasePresets/BaseAAIPreset.java
@@ -1,12 +1,18 @@
 package org.onap.simulator.presetGenerator.presets.BasePresets;
 
-/**
- * Created by itzikliderman on 27/12/2017.
- */
+import java.util.Map;
+
 public abstract class BaseAAIPreset extends BasePreset {
 
     @Override
     protected String getRootPath() {
         return "/aai/v..";
+    }
+
+    @Override
+    public Map<String, String> getRequestHeaders() {
+        Map<String, String> map = super.getRequestHeaders();
+        map.put("X-ONAP-PartnerName", "VID.VID");
+        return map;
     }
 }
