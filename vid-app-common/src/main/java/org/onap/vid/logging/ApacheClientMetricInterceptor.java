1ca4b7dd6bd484c269378ea8ec22ea376a3cb244
Eylon Malin <eylon.malin@intl.att.com>
2019-10-10 11:50:06 +0300

    base logging interceptor for apache http client
    Issue-ID: VID-253
    
    org.onap.logging.filter.spring.SpringClientFilter used
    as reference implementation
    
    Change-Id: I015bc784ab5ee2b4ee6474bdbb7a346011734a3a
    Signed-off-by: Eylon Malin <eylon.malin@intl.att.com>

---
+++ vid-app-common/src/main/java/org/onap/vid/logging/ApacheClientMetricInterceptor.java
@@ -0,0 +1,62 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * VID
+ * ================================================================================
+ * Copyright (C) 2017 - 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.vid.logging;
+
+import java.net.URI;
+import org.apache.http.HttpMessage;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.onap.logging.filter.base.AbstractMetricLogFilter;
+
+
+public abstract class ApacheClientMetricInterceptor extends AbstractMetricLogFilter<HttpRequest, HttpResponse, HttpMessage> {
+
+    @Override
+    protected void addHeader(HttpMessage httpMessage, String headerName, String headerValue) {
+        httpMessage.addHeader(headerName, headerValue);
+    }
+
+    @Override
+    protected String getTargetServiceName(HttpRequest httpRequest) {
+        return httpRequest.getRequestLine().getUri();
+    }
+
+    @Override
+    protected String getServiceName(HttpRequest httpRequest) {
+        return URI.create(httpRequest.getRequestLine().getUri()).getPath();
+    }
+
+    @Override
+    protected int getHttpStatusCode(HttpResponse httpResponse) {
+        return httpResponse.getStatusLine().getStatusCode();
+    }
+
+    @Override
+    protected String getResponseCode(HttpResponse httpResponse) {
+        return httpResponse.getStatusLine().getReasonPhrase();
+    }
+
+    @Override
+    protected String getTargetEntity(HttpRequest httpRequest) {
+        //fallback to default value that provided by AbstractMetricLogFilter
+        return null;
+    }
+}
---
+++ vid-app-common/src/test/java/org/onap/vid/logging/ApacheClientMetricInterceptorTest.java
@@ -0,0 +1,77 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * VID
+ * ================================================================================
+ * Copyright (C) 2017 - 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.vid.logging;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.message.BasicHttpResponse;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ApacheClientMetricInterceptorTest {
+
+    private ApacheClientMetricInterceptor interceptor;
+    private final String path = "https://gerrit.onap.org/r/projects/vid/branches?n=16&S=0&m=master";
+    private HttpGet request;
+    private HttpResponse response;
+
+    @BeforeMethod
+    public void before() {
+        interceptor = new ApacheClientMetricInterceptor() {};
+        request = new HttpGet(path);
+        response = new BasicHttpResponse(new ProtocolVersion("a",1,2), 200, "ok");
+    }
+
+    @Test
+    public void testAddHeader() {
+        interceptor.addHeader(request, "key", "value");
+        assertEquals(request.getFirstHeader("key").getValue(), "value");
+    }
+
+    @Test
+    public void testGetTargetServiceName() {
+        assertEquals(interceptor.getTargetServiceName(request), path);
+    }
+
+    @Test
+    public void testGetServiceName() {
+        assertEquals(interceptor.getTargetServiceName(request), path);
+    }
+
+    @Test
+    public void testGetHttpStatusCode() {
+        assertEquals(interceptor.getHttpStatusCode(response), 200);
+    }
+
+    @Test
+    public void testGetResponseCode() {
+        assertEquals(interceptor.getResponseCode(response), "ok");
+    }
+
+    @Test
+    public void testGetTargetEntity() {
+        assertNull(interceptor.getTargetEntity(request));
+    }
+}
