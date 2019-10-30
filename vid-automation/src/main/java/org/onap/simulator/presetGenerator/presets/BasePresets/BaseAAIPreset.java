a75e391c30d2b1d0d4ad86bca9acfe7f381e5310
Einat Vinouze <einat.vinouze@intl.att.com>
2019-10-30 09:43:24 +0200

    add to AAI headers in request and metric log + API test
    Issue-ID: VID-253
    Signed-off-by: Einat Vinouze <einat.vinouze@intl.att.com>
    Change-Id: Ibbdf798c085b6726badb69fa3b38cd774e762e47

--- vid-automation/src/main/java/org/onap/simulator/presetGenerator/presets/BasePresets/BaseAAIPreset.java
+++ vid-automation/src/main/java/org/onap/simulator/presetGenerator/presets/BasePresets/BaseAAIPreset.java
@@ -1,5 +1,7 @@
 package org.onap.simulator.presetGenerator.presets.BasePresets;
 
+import java.util.Map;
+
 /**
  * Created by itzikliderman on 27/12/2017.
  */
@@ -8,5 +10,14 @@ public abstract class BaseAAIPreset extends BasePreset {
     @Override
     protected String getRootPath() {
         return "/aai/v..";
+    }
+
+    @Override
+    public Map<String, String> getRequestHeaders() {
+        Map<String, String> map = super.getRequestHeaders();
+        map.put("X-ONAP-PartnerName", "VID.VID");
+        map.put("X-ONAP-InvocationID", "[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}");
+        map.put("X-ONAP-RequestID", "[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}");
+        return map;
     }
 }
