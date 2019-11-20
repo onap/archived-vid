d3189dde9e5538652235919b280a61d23a10219c
Michal Kabaj <michal.kabaj@nokia.com>
2019-11-20 21:38:54 +0100

    OperationalEnvironment improvements
    -added new unit test to AaiControllerTest
    -OperationalEnvironment and List are now immutable
    -builder improvements
    -removed redundant test
    
    Issue-ID: VID-706
    Signed-off-by: Michal Kabaj <michal.kabaj@nokia.com>
    Change-Id: I2cf0d0b124458b9306637614b3d26c85c780ca4d
    Signed-off-by: Michal Kabaj <michal.kabaj@nokia.com>

--- vid-app-common/src/main/java/org/onap/vid/aai/OperationalEnvironment.java
+++ vid-app-common/src/main/java/org/onap/vid/aai/OperationalEnvironment.java
@@ -3,13 +3,14 @@
  * VID
  * ================================================================================
  * Copyright (C) 2017 - 2019 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2019 Nokia.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,37 +21,83 @@
 
 package org.onap.vid.aai;
 
-import org.onap.vid.aai.model.RelationshipList;
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.WRITE_ONLY;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.onap.vid.aai.model.RelationshipList;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class OperationalEnvironment {
+public final class OperationalEnvironment {
 
-    private String operationalEnvironmentId;
-    private String operationalEnvironmentName;
-    private String operationalEnvironmentType;
-    private String operationalEnvironmentStatus;
-    private String tenantContext;
-    private String workloadContext;
-    private String resourceVersion;
-    private RelationshipList relationshipList;
+    private final String operationalEnvironmentId;
+    private final String operationalEnvironmentName;
+    private final String operationalEnvironmentType;
+    private final String operationalEnvironmentStatus;
+    private final String tenantContext;
+    private final String workloadContext;
+    private final String resourceVersion;
+    private final RelationshipList relationshipList;
 
-    public OperationalEnvironment() {
+    public static OperationalEnvironmentBuilder builder() {
+        return new OperationalEnvironmentBuilder();
     }
 
-    public OperationalEnvironment(OperationalEnvironmentBuilder builder) {
-        this.operationalEnvironmentId = builder.operationalEnvironmentId;
-        this.operationalEnvironmentName = builder.operationalEnvironmentName;
-        this.operationalEnvironmentType = builder.operationalEnvironmentType;
-        this.operationalEnvironmentStatus = builder.operationalEnvironmentStatus;
-        this.tenantContext = builder.tenantContext;
-        this.workloadContext = builder.workloadContext;
-        this.resourceVersion = builder.resourceVersion;
-        this.relationshipList = builder.relationshipList;
+    @JsonCreator
+    OperationalEnvironment(
+        @JsonProperty(value = "operational-environment-id", access = WRITE_ONLY) String operationalEnvironmentId,
+        @JsonProperty(value = "operational-environment-name", access = WRITE_ONLY) String operationalEnvironmentName,
+        @JsonProperty(value = "operational-environment-type", access = WRITE_ONLY) String operationalEnvironmentType,
+        @JsonProperty(value = "operational-environment-status", access = WRITE_ONLY) String operationalEnvironmentStatus,
+        @JsonProperty(value = "tenant-context", access = WRITE_ONLY) String tenantContext,
+        @JsonProperty(value = "workload-context", access = WRITE_ONLY) String workloadContext,
+        @JsonProperty(value = "resource-version", access = WRITE_ONLY) String resourceVersion,
+        @JsonProperty(value = "relationship-list", access = WRITE_ONLY) RelationshipList relationshipList) {
+        this.operationalEnvironmentId = operationalEnvironmentId;
+        this.operationalEnvironmentName = operationalEnvironmentName;
+        this.operationalEnvironmentType = operationalEnvironmentType;
+        this.operationalEnvironmentStatus = operationalEnvironmentStatus;
+        this.tenantContext = tenantContext;
+        this.workloadContext = workloadContext;
+        this.resourceVersion = resourceVersion;
+        this.relationshipList = relationshipList;
+    }
+
+    public String getOperationalEnvironmentId() {
+        return operationalEnvironmentId;
+    }
+
+    public String getOperationalEnvironmentName() {
+        return operationalEnvironmentName;
+    }
+
+    public String getOperationalEnvironmentType() {
+        return operationalEnvironmentType;
+    }
+
+    public String getOperationalEnvironmentStatus() {
+        return operationalEnvironmentStatus;
+    }
+
+    public String getTenantContext() {
+        return tenantContext;
+    }
+
+    public String getWorkloadContext() {
+        return workloadContext;
+    }
+
+    public String getResourceVersion() {
+        return resourceVersion;
+    }
+
+    public RelationshipList getRelationshipList() {
+        return relationshipList;
     }
 
     public static class OperationalEnvironmentBuilder {
+
         private String operationalEnvironmentId;
         private String operationalEnvironmentName;
         private String operationalEnvironmentType;
@@ -58,127 +105,65 @@ public class OperationalEnvironment {
         private String tenantContext;
         private String workloadContext;
         private String resourceVersion;
+
         private RelationshipList relationshipList;
 
-        public OperationalEnvironmentBuilder setOperationalEnvironmentId(
-                String operationalEnvironmentId) {
+        public OperationalEnvironmentBuilder withOperationalEnvironmentId(
+            String operationalEnvironmentId) {
             this.operationalEnvironmentId = operationalEnvironmentId;
             return this;
         }
 
-        public OperationalEnvironmentBuilder setOperationalEnvironmentName(
-                String operationalEnvironmentName) {
+        public OperationalEnvironmentBuilder withOperationalEnvironmentName(
+            String operationalEnvironmentName) {
             this.operationalEnvironmentName = operationalEnvironmentName;
             return this;
         }
 
-        public OperationalEnvironmentBuilder setOperationalEnvironmentType(
-                String operationalEnvironmentType) {
+        public OperationalEnvironmentBuilder withOperationalEnvironmentType(
+            String operationalEnvironmentType) {
             this.operationalEnvironmentType = operationalEnvironmentType;
             return this;
         }
 
-        public OperationalEnvironmentBuilder setOperationalEnvironmentStatus(
-                String operationalEnvironmentStatus) {
+        public OperationalEnvironmentBuilder withOperationalEnvironmentStatus(
+            String operationalEnvironmentStatus) {
             this.operationalEnvironmentStatus = operationalEnvironmentStatus;
             return this;
         }
 
-        public OperationalEnvironmentBuilder setTenantContext(String tenantContext) {
+        public OperationalEnvironmentBuilder withTenantContext(String tenantContext) {
             this.tenantContext = tenantContext;
             return this;
         }
 
-        public OperationalEnvironmentBuilder setWorkloadContext(String workloadContext) {
+        public OperationalEnvironmentBuilder withWorkloadContext(String workloadContext) {
             this.workloadContext = workloadContext;
             return this;
         }
 
-        public OperationalEnvironmentBuilder setResourceVersion(String resourceVersion) {
+        public OperationalEnvironmentBuilder withResourceVersion(String resourceVersion) {
             this.resourceVersion = resourceVersion;
             return this;
         }
 
-        public OperationalEnvironmentBuilder setRelationshipList(
-                RelationshipList relationshipList) {
+        public OperationalEnvironmentBuilder withRelationshipList(
+            RelationshipList relationshipList) {
             this.relationshipList = relationshipList;
             return this;
         }
 
-        public OperationalEnvironment createOperationalEnvironment() {
-            return new OperationalEnvironment(this);
+        public OperationalEnvironment build() {
+            return new OperationalEnvironment(operationalEnvironmentId,
+                operationalEnvironmentName,
+                operationalEnvironmentType,
+                operationalEnvironmentStatus,
+                tenantContext,
+                workloadContext,
+                resourceVersion,
+                relationshipList);
         }
+
     }
 
-    public String getOperationalEnvironmentId() {
-        return operationalEnvironmentId;
-    }
-
-    @JsonProperty("operational-environment-id")
-    public void setJsonOperationalEnvironmentId(String operationalEnvironmentId) {
-        this.operationalEnvironmentId = operationalEnvironmentId;
-    }
-
-    public String getOperationalEnvironmentName() {
-        return operationalEnvironmentName;
-    }
-
-    @JsonProperty("operational-environment-name")
-    public void setJsonOperationalEnvironmentName(String operationalEnvironmentName) {
-        this.operationalEnvironmentName = operationalEnvironmentName;
-    }
-
-    public String getOperationalEnvironmentType() {
-        return operationalEnvironmentType;
-    }
-
-    @JsonProperty("operational-environment-type")
-    public void setJsonOperationalEnvironmentType(String operationalEnvironmentType) {
-        this.operationalEnvironmentType = operationalEnvironmentType;
-    }
-
-    public String getOperationalEnvironmentStatus() {
-        return operationalEnvironmentStatus;
-    }
-
-    @JsonProperty("operational-environment-status")
-    public void setJsonOperationalEnvironmentStatus(String operationalEnvironmentStatus) {
-        this.operationalEnvironmentStatus = operationalEnvironmentStatus;
-    }
-
-    public String getTenantContext() {
-        return tenantContext;
-    }
-
-    @JsonProperty("tenant-context")
-    public void setJsonTenantContext(String tenantContext) {
-        this.tenantContext = tenantContext;
-    }
-
-    public String getWorkloadContext() {
-        return workloadContext;
-    }
-
-    @JsonProperty("workload-context")
-    public void setJsonWorkloadContext(String workloadContext) {
-        this.workloadContext = workloadContext;
-    }
-
-    public String getResourceVersion() {
-        return resourceVersion;
-    }
-
-    @JsonProperty("resource-version")
-    public void setJsonResourceVersion(String resourceVersion) {
-        this.resourceVersion = resourceVersion;
-    }
-
-    public RelationshipList getRelationshipList() {
-        return relationshipList;
-    }
-
-    @JsonProperty("relationship-list")
-    public void setJsonRelationshipList(RelationshipList relationshipList) {
-        this.relationshipList = relationshipList;
-    }
 }
--- vid-app-common/src/test/java/org/onap/vid/aai/OperationalEnvironmentTest.java
+++ vid-app-common/src/test/java/org/onap/vid/aai/OperationalEnvironmentTest.java
@@ -22,73 +22,94 @@
 package org.onap.vid.aai;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Assert;
-import org.onap.vid.aai.model.RelationshipList;
-import org.testng.annotations.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.ArrayList;
-
-import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.onap.vid.aai.model.RelationshipList;
+import org.testng.annotations.Test;
 
 
 public class OperationalEnvironmentTest {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    private static final String OPERATIONAL_ENVIRONMENT_TEST = "{\n" +
-            "\"operational-environment-id\": \"environmentId\",\n" +
-            "\"operational-environment-name\": \"environmentName\",\n" +
-            "\"operational-environment-type\": \"environmentType\",\n" +
-            "\"operational-environment-status\": \"environmentStatus\",\n" +
-            "\"tenant-context\": \"tenantContext\",\n" +
-            "\"workload-context\": \"workloadContext\",\n" +
-            "\"resource-version\": \"resourceVersion\",\n" +
-            "\"relationship-list\": {\n" +
-            "\"relationship\": []\n" +
-            "}\n" +
-            "}";
+    private static final String OPERATIONAL_ENVIRONMENT_JSON_DASHES = "{\n" +
+        "\"operational-environment-id\": \"testEnvironmentId\",\n" +
+        "\"operational-environment-name\": \"testEnvironmentName\",\n" +
+        "\"operational-environment-type\": \"testEnvironmentType\",\n" +
+        "\"operational-environment-status\": \"testEnvironmentStatus\",\n" +
+        "\"tenant-context\": \"testTenantContext\",\n" +
+        "\"workload-context\": \"testWorkloadContext\",\n" +
+        "\"resource-version\": \"testResourceVersion\",\n" +
+        "\"relationship-list\": {\n" +
+        "\"relationship\": []\n" +
+        "}\n" +
+        "}";
+
+    private static final String OPERATIONAL_ENVIRONMENT_JSON_CAMELCASE = "{\n" +
+        "\"operationalEnvironmentId\": \"testEnvironmentId\",\n" +
+        "\"operationalEnvironmentName\": \"testEnvironmentName\",\n" +
+        "\"operationalEnvironmentType\": \"testEnvironmentType\",\n" +
+        "\"operationalEnvironmentStatus\": \"testEnvironmentStatus\",\n" +
+        "\"tenantContext\": \"testTenantContext\",\n" +
+        "\"workloadContext\": \"testWorkloadContext\",\n" +
+        "\"resourceVersion\": \"testResourceVersion\",\n" +
+        "\"relationshipList\": {\n" +
+        "\"relationship\": []\n" +
+        "}\n" +
+        "}";
 
     @Test
-    public void shouldCreateProperOperationalEnvironmentWithConstructor(){
+    public void shouldProperlyConvertJsonToOperationalEnvironment_whenJsonPropertyNamesContainDashSeparators()
+        throws IOException {
+        assertOperationalEnvironmentDeserialization(OPERATIONAL_ENVIRONMENT_JSON_DASHES);
+    }
+
+    @Test
+    public void shouldProperlyConvertJsonToOperationalEnvironment_whenJsonPropertyNamesAreCamelCase()
+        throws IOException {
+        assertOperationalEnvironmentDeserialization(OPERATIONAL_ENVIRONMENT_JSON_CAMELCASE);
+    }
+
+    private void assertOperationalEnvironmentDeserialization(String operationalEnvironmentTestDashes)
+        throws IOException {
+        OperationalEnvironment operationalEnvironment =
+            OBJECT_MAPPER.readValue(operationalEnvironmentTestDashes, OperationalEnvironment.class);
+
+        assertThat(operationalEnvironment.getOperationalEnvironmentId()).isEqualTo("testEnvironmentId");
+        assertThat(operationalEnvironment.getOperationalEnvironmentName()).isEqualTo("testEnvironmentName");
+        assertThat(operationalEnvironment.getOperationalEnvironmentType()).isEqualTo("testEnvironmentType");
+        assertThat(operationalEnvironment.getOperationalEnvironmentStatus()).isEqualTo("testEnvironmentStatus");
+        assertThat(operationalEnvironment.getTenantContext()).isEqualTo("testTenantContext");
+        assertThat(operationalEnvironment.getWorkloadContext()).isEqualTo("testWorkloadContext");
+        assertThat(operationalEnvironment.getResourceVersion()).isEqualTo("testResourceVersion");
+        assertThat(operationalEnvironment.getRelationshipList().getRelationship()).hasSize(0);
+    }
+
+    @Test
+    public void shouldSerializeToJson_usingActualPropertyNameAsJsonLogicalProperty() throws JsonProcessingException {
         RelationshipList relationshipList = new RelationshipList();
         relationshipList.relationship = new ArrayList<>();
 
-        OperationalEnvironment operationalEnvironment =
-                new OperationalEnvironment.OperationalEnvironmentBuilder()
-                        .setOperationalEnvironmentId("testId")
-                        .setOperationalEnvironmentName("testEnvName")
-                        .setOperationalEnvironmentType("testEnvType")
-                        .setOperationalEnvironmentStatus("testEnvStatus")
-                        .setTenantContext("testTenant").setWorkloadContext("testWorkload")
-                        .setResourceVersion("testResource").setRelationshipList(relationshipList)
-                        .createOperationalEnvironment();
+        OperationalEnvironment operationalEnvironment = createOperationalEnvironment(relationshipList);
 
-        assertThat(operationalEnvironment.getOperationalEnvironmentId()).isEqualTo("testId");
-        assertThat(operationalEnvironment.getWorkloadContext()).isEqualTo("testWorkload");
-        assertThat(operationalEnvironment.getRelationshipList().getRelationship()).hasSize(0);
-        assertThat(operationalEnvironment.getResourceVersion()).isEqualTo("testResource");
-        assertThat(operationalEnvironment.getTenantContext()).isEqualTo("testTenant");
-        assertThat(operationalEnvironment.getOperationalEnvironmentType()).isEqualTo("testEnvType");
-        assertThat(operationalEnvironment.getOperationalEnvironmentStatus()).isEqualTo("testEnvStatus");
-        assertThat(operationalEnvironment.getOperationalEnvironmentName()).isEqualTo("testEnvName");
+        assertThat(OPERATIONAL_ENVIRONMENT_JSON_CAMELCASE)
+            .isEqualToIgnoringWhitespace(OBJECT_MAPPER.writeValueAsString(operationalEnvironment));
     }
 
-    @Test
-    public void shouldProperlyConvertJsonToOperationalEnvironment() throws IOException {
-        OperationalEnvironment operationalEnvironment =
-                OBJECT_MAPPER.readValue(OPERATIONAL_ENVIRONMENT_TEST, OperationalEnvironment.class);
-
-        assertThat(operationalEnvironment.getOperationalEnvironmentId()).isEqualTo("environmentId");
-        assertThat(operationalEnvironment.getWorkloadContext()).isEqualTo("workloadContext");
-        assertThat(operationalEnvironment.getRelationshipList().getRelationship()).hasSize(0);
-        assertThat(operationalEnvironment.getResourceVersion()).isEqualTo("resourceVersion");
-        assertThat(operationalEnvironment.getTenantContext()).isEqualTo("tenantContext");
-        assertThat(operationalEnvironment.getOperationalEnvironmentType()).isEqualTo("environmentType");
-        assertThat(operationalEnvironment.getOperationalEnvironmentStatus()).isEqualTo("environmentStatus");
-        assertThat(operationalEnvironment.getOperationalEnvironmentName()).isEqualTo("environmentName");
+    private OperationalEnvironment createOperationalEnvironment(RelationshipList relationshipList) {
+        return new OperationalEnvironment.OperationalEnvironmentBuilder()
+            .withOperationalEnvironmentId("testEnvironmentId")
+            .withOperationalEnvironmentName("testEnvironmentName")
+            .withOperationalEnvironmentType("testEnvironmentType")
+            .withOperationalEnvironmentStatus("testEnvironmentStatus")
+            .withTenantContext("testTenantContext")
+            .withWorkloadContext("testWorkloadContext")
+            .withResourceVersion("testResourceVersion")
+            .withRelationshipList(relationshipList)
+            .build();
     }
-
 }
