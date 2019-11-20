699f5ba57ffbd7dc5b9afb8fe9540be37db05c56
Yoav Schneiderman <yoav.schneiderman@intl.att.com>
2019-11-20 16:50:45 +0200

    adding basic implementation to multi select dropdown. with init call , convert to multi select model
    Issue-ID: VID-584
    Change-Id: Id87f59fff128e277d9158b83f3908754375c8b01
    Signed-off-by: Yoav Schneiderman <yoav.schneiderman@intl.att.com>

--- vid-webpack-master/cypress/support/steps/genericForm/genericFormAction.steps.ts
+++ vid-webpack-master/cypress/support/steps/genericForm/genericFormAction.steps.ts
@@ -1,12 +1,29 @@
 declare namespace Cypress {
   interface Chainable {
     genericFormSubmitForm: typeof genericFormSubmitForm
+    selelctPlatformValue: typeof selelctPlatformValue
   }
 }
 
-function genericFormSubmitForm() : Chainable<any>  {
-  return  cy.getElementByDataTestsId('form-set').click({force: true});
+
+
+
+function selelctPlatformValue(isDropdown: boolean, selectOption: string){
+  if (isDropdown) {
+    cy.selectDropdownOptionByText('platform', selectOption);
+  } else {
+    cy.getElementByDataTestsId("multi-selectPlatform").get('.c-btn').click({force: true})
+      .getElementByDataTestsId(`multi-selectPlatform-${selectOption}`).click()
+      .getElementByDataTestsId("multi-selectPlatform").get('.c-btn').click({force: true});
+
+  }
+}
+
+
+function genericFormSubmitForm(): Chainable<any> {
+  return cy.getElementByDataTestsId('form-set').click({force: true});
 }
 
 
 Cypress.Commands.add('genericFormSubmitForm', genericFormSubmitForm);
+Cypress.Commands.add('selelctPlatformValue', selelctPlatformValue);
