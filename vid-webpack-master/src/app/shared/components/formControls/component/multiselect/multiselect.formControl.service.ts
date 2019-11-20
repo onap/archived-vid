f16a52940729e95c63e49fef7c36fcfac1e0f4ec
Yoav Schneiderman <yoav.schneiderman@intl.att.com>
2019-11-20 16:50:45 +0200

    adding impl to view edit + unit tests + cypress with multi select (s options)
    Issue-ID: VID-584
    Change-Id: Id87f59fff128e277d9158b83f3908754375c8b01
    Signed-off-by: Yoav Schneiderman <yoav.schneiderman@intl.att.com>
    Signed-off-by: Einat Vinouze <einat.vinouze@intl.att.com>

---
+++ vid-webpack-master/src/app/shared/components/formControls/component/multiselect/multiselect.formControl.service.ts
@@ -0,0 +1,50 @@
+import {Injectable} from "@angular/core";
+import {MultiselectFormControl} from "../../../../models/formControlModels/multiselectFormControl.model";
+import {MultiSelectItem} from "./multiselect.model";
+import * as _ from "lodash";
+
+
+@Injectable()
+export class MultiselectFormControlService {
+
+  convertOriginalItems = (data : MultiselectFormControl) : Promise<MultiSelectItem[]> => {
+    return new Promise<MultiSelectItem[]>((resolve) =>{
+      let result: MultiSelectItem[] = [];
+      if(data.options$) {
+        let index: number = 1;
+        data.options$.map((originalItems: any) => {
+          result.push(new MultiSelectItem(index, originalItems[data.ngValue], originalItems[data.selectedFieldName]));
+          index++;
+        });
+      }
+      resolve(result);
+    })
+  };
+
+
+  convertOptionsToHashMap = (config : MultiselectFormControl) => {
+    let index = 1;
+    return _.reduce(config.options$ , (obj, param: any ) => {
+      param.index = index;
+      index++;
+      obj[param[config.ngValue]] = param;
+      return obj;
+    }, {});
+  };
+
+  convertSelectedItems(data : MultiselectFormControl) : Promise<MultiSelectItem[]>{
+    return new Promise<MultiSelectItem[]>((resolve) =>{
+      let result: MultiSelectItem[] = [];
+      const hashMap = this.convertOptionsToHashMap(data);
+
+      if(data.options$ && data.value) {
+        const convertArray = data.convertOriginalDataToArray ? data.convertOriginalDataToArray(data.value) : data.value;
+        convertArray.map((itemId) => {
+          const uniqueIdentifier = itemId.trim();
+          result.push(new MultiSelectItem(hashMap[uniqueIdentifier].index, hashMap[uniqueIdentifier][data.ngValue], hashMap[uniqueIdentifier][data.selectedFieldName]));
+        });
+      }
+      resolve(result);
+    });
+  }
+}
