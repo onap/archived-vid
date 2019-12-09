c3e21b6574168a2fe92b6371a3878febd24375b6
Yoav Schneiderman <yoav.schneiderman@intl.att.com>
2019-12-09 16:42:21 +0200

    Adding unlimited max value to VNF, NETWORK
    Issue-ID: VID-726
    Signed-off-by: Yoav Schneiderman <yoav.schneiderman@intl.att.com>
    Change-Id: I0c3d503c8e4f6cb14081de8f6a619a67eee080b7
    Signed-off-by: Yoav Schneiderman <yoav.schneiderman@intl.att.com>

--- vid-webpack-master/src/app/shared/models/vnfModel.ts
+++ vid-webpack-master/src/app/shared/models/vnfModel.ts
@@ -3,24 +3,34 @@ import {
   Level1ModelProperties,
   Level1ModelResponseInterface
 } from "./nodeModel";
+import * as _ from "lodash";
 
 
-
-export interface VnfProperties extends Level1ModelProperties{
+export interface VnfProperties extends Level1ModelProperties {
   ecomp_generated_naming: string;
 }
 
-export interface VNFModelResponseInterface extends Level1ModelResponseInterface{
+export interface VNFModelResponseInterface extends Level1ModelResponseInterface {
   properties: VnfProperties;
 }
 
-export class VNFModel extends Level1Model{
+export class VNFModel extends Level1Model {
   properties: VnfProperties;
 
-  constructor(vnfJson?: VNFModelResponseInterface) {
+  constructor(vnfJson?: VNFModelResponseInterface, flags?: { [key: string]: boolean }) {
     super(vnfJson);
     if (vnfJson) {
       this.properties = vnfJson.properties;
+      this.max = this.getMax(this.properties, flags);
+    }
+  }
+
+
+  getMax(properties, flags: { [key: string]: boolean }) {
+    if (flags && !!flags['FLAG_2002_UNLIMITED_MAX']) {
+      return !_.isNil(properties) && !_.isNil(properties.max_instances) ? properties.max_instances : null;
+    } else {
+      return properties.max_instances || 1
     }
   }
 
