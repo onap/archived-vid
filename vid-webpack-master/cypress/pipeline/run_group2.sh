e60bd9dff5a700e96c0a9a900cdaa8085fe708c0
Eylon Malin <eylon.malin@intl.att.com>
2020-01-06 11:31:20 +0200

    cypress test - split cypress test to 3 groups
    Issue-ID: VID-740
    Change-Id: I772e26cc71339027d8a273acbb256ea1a8d81a11
    Signed-off-by: Eylon Malin <eylon.malin@intl.att.com>

--- vid-webpack-master/cypress/pipeline/run_group2.sh
+++ vid-webpack-master/cypress/pipeline/run_group2.sh
@@ -5,13 +5,13 @@
 set -x
 
 CYPRESS_HOME_DIR=$1
-TESTS_GROUP_FILE_1=cypress/pipeline/group1.txt
-TESTS_GROUP_FILE=cypress/pipeline/group2.generated.txt
+REMAIN_TESTS_FILE=cypress/pipeline/remain.generated.txt
+ALL_TESTS_IN_GROUPS_FILE=cypress/pipeline/all_tests.txt
 
 cd ${CYPRESS_HOME_DIR}
-
+cat cypress/pipeline/group?.txt | sort > ${ALL_TESTS_IN_GROUPS_FILE}
 # make group2 by "negating" group1.txt
-ls -1 cypress/integration/*/*.e2e.ts | comm -3 - ${TESTS_GROUP_FILE_1} > ${TESTS_GROUP_FILE}
+ls -1 cypress/integration/*/*.e2e.ts | comm -3 - ${ALL_TESTS_IN_GROUPS_FILE} > ${REMAIN_TESTS_FILE}
 
-cat ${TESTS_GROUP_FILE}
-npm run cypress:headless --max-old-space-size=4096 -- --spec=$(cat ${TESTS_GROUP_FILE} | tr '\n' ',')
+cat ${REMAIN_TESTS_FILE}
+npm run cypress:headless --max-old-space-size=4096 -- --spec=$(cat ${REMAIN_TESTS_FILE} | tr '\n' ',')
