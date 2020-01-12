1b6489ab0b08c988369087a3ca3d467a5ef7dfec
Eylon Malin <eylon.malin@intl.att.com>
2020-01-12 16:09:48 +0200

    fix cypress test - rename from hasTemplates to isInstantiationTemplateExists
    Issue-ID: VID-741
    Change-Id: I82ac3053246988cddfaeebe6d191a271cdd3d206
    Signed-off-by: Eylon Malin <eylon.malin@intl.att.com>

--- vid-webpack-master/cypress/integration/iFrames/browse-sdc.e2e.ts
+++ vid-webpack-master/cypress/integration/iFrames/browse-sdc.e2e.ts
@@ -63,13 +63,13 @@ describe('Browse SDC', function () {
 
   });
 
-  it(`browse sdc should open instantiation template modal if service hasTemplate is true`, function () {
+  it(`browse sdc should open instantiation template modal if service isInstantiationTemplateExists is true`, function () {
     const SERVICE_MODEL_ID: string = '74fa72dd-012b-49c3-800d-06b12bcaf1a0';
 
     cy.readFile('cypress/support/jsonBuilders/mocks/jsons/bug616888/list-services.json').then((res) => {
-      res.services = res.services.map((service: { uuid: string, hasTemplate: boolean }) => {
+      res.services = res.services.map((service: { uuid: string, isInstantiationTemplateExists: boolean }) => {
         if (service.uuid === SERVICE_MODEL_ID) {
-          service.hasTemplate = true;
+          service.isInstantiationTemplateExists = true;
         }
         return service;
       });
