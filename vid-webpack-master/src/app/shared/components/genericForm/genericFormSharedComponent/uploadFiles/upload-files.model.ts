8c996bf77342aa2ebb957a36a1df8ef664857e7f
Yoav Schneiderman <yoav.schneiderman@intl.att.com>
2020-01-22 11:07:00 +0200

    SDN-c pre load upload files ( JUST UI WITHOUT BE) Issue-ID: VID-752
    Signed-off-by: Yoav Schneiderman <yoav.schneiderman@intl.att.com>
    Change-Id: I96ef914bfe37d82da20085227b930523fcdba8fc
    Signed-off-by: Yoav Schneiderman <yoav.schneiderman@intl.att.com>

---
+++ vid-webpack-master/src/app/shared/components/genericForm/genericFormSharedComponent/uploadFiles/upload-files.model.ts
@@ -0,0 +1,31 @@
+import {FileItem} from "ng2-file-upload";
+import {Observable} from "rxjs";
+
+export class UploadFilesModel {
+  uploadText?: string;
+
+  /*********************************************************************
+   Implement success method - run after uploadMethod return true result
+   **********************************************************************/
+  onSuccess?: (...args) => void;
+
+  /*********************************************************************
+   Implement failed method - run after uploadMethod return false result
+   **********************************************************************/
+  onFailed?: (...args) => void;
+
+  /*********************************************************************************
+   Implement upload method and return the upload result status (false/true)
+   *********************************************************************************/
+  uploadMethod: (file: FileItem, ...args) => Promise<boolean>;
+
+  /********************************
+   Should upload file be disabled
+   ********************************/
+  isDisabled?: (...args) => boolean;
+
+  /********************************
+   a tag data test id
+   ********************************/
+  dataTestId : string;
+}
