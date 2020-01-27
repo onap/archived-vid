abb69004c16707e1dafa7a75050e8c026de9fe31
Ittay Stern <ittay.stern@att.com>
2020-01-27 19:09:19 +0200

    RoleValidatorsComposer composite several RoleValidators together
    Issue-ID: VID-758
    
    Change-Id: I7bd5a4ebc8a0a451716af09cc76e6db6cd5362f7
    Signed-off-by: Ittay Stern <ittay.stern@att.com>

---
+++ vid-app-common/src/main/java/org/onap/vid/roles/RoleValidatorsComposer.kt
@@ -0,0 +1,16 @@
+package org.onap.vid.roles
+
+class RoleValidatorsComposer(private vararg val roleValidators: RoleValidator) : RoleValidator {
+
+    constructor(roleValidators: Collection<RoleValidator>) : this(*roleValidators.toTypedArray())
+
+    override fun isServicePermitted(p: WithPermissionProperties): Boolean =
+            roleValidators.any { it.isServicePermitted(p) }
+
+    override fun isSubscriberPermitted(subscriberName: String?): Boolean =
+            roleValidators.any { it.isSubscriberPermitted(subscriberName) }
+
+    override fun isTenantPermitted(globalCustomerId: String?, serviceType: String?, tenantName: String?): Boolean =
+            roleValidators.any { it.isTenantPermitted(globalCustomerId, serviceType, tenantName) }
+
+}
