16518f0ac0e4abd085efea994672477f7ef9ee1e
Alexey Sandler <alexey.sandler@intl.att.com>
2020-04-20 12:57:09 +0300

    Add drop down Source subscriber name on the PMC instantiation form
    Add the "Source Subscriber Name" drop down for the source VNF on the PMC (Port Mirroring Configuration) instantiation form.
    Issue-ID: VID-809
    Change-Id: Id36ea128363444a1339bec88df7276f6886d7dd5
    
    Signed-off-by: Einat Vinouze <einat.vinouze@intl.att.com>
    Change-Id: Iab01675e815bbc9809c67e31e475613973d868ea
    Signed-off-by: Einat Vinouze <einat.vinouze@intl.att.com>
    Signed-off-by: Alexey Sandler <alexey.sandler@intl.att.com>
    Signed-off-by: Einat Vinouze <einat.vinouze@intl.att.com>

--- vid-app-common/src/main/webapp/app/vid/scripts/controller/ServiceProxyConfigController.js
+++ vid-app-common/src/main/webapp/app/vid/scripts/controller/ServiceProxyConfigController.js
@@ -60,12 +60,27 @@ appDS2.controller(
 
         $scope.serviceTypes = [];
 
-        function init() {
-            loadServiceTypes();
+        $scope.isSourceSubscriberEnabled = function() {
+            return featureFlags.isOn(COMPONENT.FEATURE_FLAGS.FLAG_2006_PORT_MIRRORING_LET_SELECTING_SOURCE_SUBSCRIBER);
+        };
 
+        function init() {
+            loadSourceSubscriber();
             generateMetadata(sourceServiceProxy);
             generateMetadata(collectorServiceProxy);
 
+        }
+
+        function loadSourceSubscriber() {
+            AaiService.getSubList(function (response) {
+                $scope.sourceSubscribers = response;
+                $scope.sourceSubscriber = response.find((subscriber)=>{
+                    return subscriber.globalCustomerId === DataService.getGlobalCustomerId();
+                });
+                $scope.loadServiceTypes($scope.sourceSubscriber);
+            }, function (response) { // failure
+                $scope.sourceSubscribers = [];
+            });
         }
 
         function setDefaultCollectorServiceType() {
@@ -98,9 +113,9 @@ appDS2.controller(
         };
 
 
-        function loadServiceTypes() {
-            const subscriberId = DataService.getGlobalCustomerId();
-            AaiService.getSubscriberServiceTypes(subscriberId)
+        $scope.loadServiceTypes = function(selectedSubscriber) {
+            $scope.sourceSubscriber = selectedSubscriber;
+            AaiService.getSubscriberServiceTypes(selectedSubscriber['globalCustomerId'])
                 .then(handleGetServiceTypesResponse)
                 .catch(function (error) {
                     $log.error(error);
@@ -273,16 +288,17 @@ appDS2.controller(
         function loadSourceProxies() {
             var serviceProxy = serviceProxiesList[(sourceServiceProxy.serviceList)[0]];
             var selectedServiceType = $scope.sourceServiceType['service-type'];
-            loadProxyInstances(sourceServiceProxy, selectedServiceType, serviceProxy);
+            var selectedSubscriberId = $scope.sourceSubscriber['globalCustomerId'] || DataService.getGlobalCustomerId();
+            loadProxyInstances(sourceServiceProxy, selectedServiceType, selectedSubscriberId, serviceProxy);
         }
 
         function loadCollectorProxies() {
             var serviceProxy = serviceProxiesList[(collectorServiceProxy.serviceList)[0]];
             var selectedServiceType = $scope.collectorServiceType['service-type'];
-            loadProxyInstances(collectorServiceProxy, selectedServiceType, serviceProxy);
+            loadProxyInstances(collectorServiceProxy, selectedServiceType, DataService.getGlobalCustomerId() , serviceProxy);
         }
 
-        function loadProxyInstances(service, serviceType, serviceProxy) {
+        function loadProxyInstances(service, serviceType, selectedSubscriberId, serviceProxy) {
             $scope[service.instanceListScopePropertyName] = null;
             var configNodeTemplateFields = DataService.getPortMirroningConfigFields();
             if (service.name === 'collectorInstanceName' && $scope.configurationByPolicy) {
@@ -307,7 +323,7 @@ appDS2.controller(
                     });
             } else {
                 AaiService.getVnfInstancesList(
-                    DataService.getGlobalCustomerId(),
+                    selectedSubscriberId,
                     serviceType,
                     serviceProxy.sourceModelUuid,
                     serviceProxy.sourceModelInvariant,
