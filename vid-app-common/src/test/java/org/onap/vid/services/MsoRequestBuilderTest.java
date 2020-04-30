366df966aac2457abb1f311d84ceabbc4b5a3373
Amichai Hemli <amichai.hemli@intl.att.com>
2020-04-30 10:53:23 +0300

    When cloud region is missing then omit the cloudConfiguration object
    Issue-ID: VID-820
    Signed-off-by: Amichai Hemli <amichai.hemli@intl.att.com>
    Change-Id: I0780649982c0a1ae0a5bc56da438d422a710e4bf

--- vid-app-common/src/test/java/org/onap/vid/services/MsoRequestBuilderTest.java
+++ vid-app-common/src/test/java/org/onap/vid/services/MsoRequestBuilderTest.java
@@ -659,6 +659,63 @@ public class MsoRequestBuilderTest extends AsyncInstantiationBaseTest {
         assertThat(generatedVfModuleReplaceRequest(retainAssignments, retainVolumeGroups, null), jsonEquals(expected).when(IGNORING_ARRAY_ORDER));
     }
 
+    @Test
+    public void generateReplaceVfModuleRequestWithoutCloudConfiguration_verifyResultAsExpected() {
+        assertThat(generatedVfModuleReplaceRequestWithoutCloudConfiguration(null, null, null), jsonEquals(getReplaceVfModulePayloadToMsoWithoutCloudConfiguration()));
+    }
+
+    private String getReplaceVfModulePayloadToMsoWithoutCloudConfiguration() {
+        return "{" +
+                "  \"requestDetails\": {" +
+                "    \"requestInfo\": {" +
+                "      \"source\": \"VID\"," +
+                "      \"requestorId\": \"az2016\"," +
+                "      \"instanceName\": \"replace_module\"," +
+                "      \"suppressRollback\": false" +
+                "    }," +
+                "    \"relatedInstanceList\": [{" +
+                "      \"relatedInstance\": {" +
+                "        \"instanceId\": \"e9993045-cc96-4f3f-bf9a-71b2a400a956\"," +
+                "        \"modelInfo\": {" +
+                "          \"modelVersionId\": \"newest-model-uuid-service\"," +
+                "          \"modelVersion\": \"newest-model-version-service\"," +
+                "          \"modelName\": \"newest-model-name-service\"," +
+                "          \"modelInvariantId\": \"b16a9398-ffa3-4041-b78c-2956b8ad9c7b\"," +
+                "          \"modelType\": \"service\"" +
+                "        }" +
+                "      }" +
+                "    }, {" +
+                "      \"relatedInstance\": {" +
+                "        \"instanceId\": \"5c9c2896-1fe6-4055-b7ec-d0a01e5f9bf5\"," +
+                "        \"modelInfo\": {" +
+                "          \"modelName\": \"newest-model-name-vnf\"," +
+                "          \"modelVersion\": \"newest-model-version-vnf\"," +
+                "          \"modelInvariantId\": \"23122c9b-dd7f-483f-bf0a-e069303db2f7\"," +
+                "          \"modelType\": \"vnf\"," +
+                "          \"modelCustomizationName\": \"newest-model-customization-name-vnf\"," +
+                "          \"modelVersionId\": \"newest-model-uuid-vnf\"," +
+                "          \"modelCustomizationId\": \"newest-model-customization-uuid-vnf\"" +
+                "        }" +
+                "      }" +
+                "    }]," +
+                "    \"requestParameters\": {" +
+                "      \"userParams\": []," +
+                "      \"usePreload\": false," +
+                "      \"testApi\": \"GR_API\"" +
+                "    }," +
+                "    \"modelInfo\": {" +
+                "      \"modelName\": \"newest-model-name-vfm\"," +
+                "      \"modelVersion\": \"newest-model-version-vfm\"," +
+                "      \"modelInvariantId\": \"f7a867f2-596b-4f4a-a128-421e825a6190\"," +
+                "      \"modelType\": \"vfModule\"," +
+                "      \"modelVersionId\": \"newest-model-uuid-vfm\"," +
+                "      \"modelCustomizationId\": \"newest-model-customization-uuid-vfm\"," +
+                "      \"modelCustomizationName\":\"newest-model-customization-name-vfm\"" +
+                "    }" +
+                "  }" +
+                "}";
+    }
+
     private RequestDetailsWrapper<VfModuleOrVolumeGroupRequestDetails> generatedVfModuleReplaceRequest(
         Boolean retainAssignments, Boolean retainVolumeGroups, List<UserParamNameAndValue> supplementaryParams) {
         when(featureManager.isActive(Features.FLAG_1810_CR_ADD_CLOUD_OWNER_TO_MSO_REQUEST)).thenReturn(true);
@@ -677,6 +734,27 @@ public class MsoRequestBuilderTest extends AsyncInstantiationBaseTest {
         return msoRequestBuilder.generateVfModuleReplaceRequest(vfModuleDetails, serviceModelInfo,
             "e9993045-cc96-4f3f-bf9a-71b2a400a956", vnfModelInfo,
             "5c9c2896-1fe6-4055-b7ec-d0a01e5f9bf5", null, "az2016", "GR_API"
+        );
+    }
+
+    private RequestDetailsWrapper<VfModuleOrVolumeGroupRequestDetails> generatedVfModuleReplaceRequestWithoutCloudConfiguration(
+            Boolean retainAssignments, Boolean retainVolumeGroups, List<UserParamNameAndValue> supplementaryParams) {
+        when(featureManager.isActive(Features.FLAG_1810_CR_ADD_CLOUD_OWNER_TO_MSO_REQUEST)).thenReturn(true);
+        when(aaiClient.getCloudOwnerByCloudRegionId("regionOne")).thenReturn("irma-aic");
+
+        ModelInfo vfModuleModelInfo = createVfModuleModelInfo("newest-model-name-vfm", "newest-model-version-vfm", "newest-model-uuid-vfm",
+                "f7a867f2-596b-4f4a-a128-421e825a6190", "newest-model-customization-uuid-vfm","newest-model-customization-name-vfm" );
+
+        VfModule vfModuleDetails = createVfModuleForReplace(vfModuleModelInfo, "replace_module", null, null,
+                retainAssignments, retainVolumeGroups, supplementaryParams);
+
+        ModelInfo serviceModelInfo = createServiceModelInfo("newest-model-name-service", "newest-model-version-service", "newest-model-uuid-service", "b16a9398-ffa3-4041-b78c-2956b8ad9c7b", null, null );
+
+        ModelInfo vnfModelInfo = createVnfModelInfo("newest-model-name-vnf", "newest-model-version-vnf", "newest-model-uuid-vnf", "23122c9b-dd7f-483f-bf0a-e069303db2f7", "newest-model-customization-uuid-vnf", "newest-model-customization-name-vnf" );
+
+        return msoRequestBuilder.generateVfModuleReplaceRequest(vfModuleDetails, serviceModelInfo,
+                "e9993045-cc96-4f3f-bf9a-71b2a400a956", vnfModelInfo,
+                "5c9c2896-1fe6-4055-b7ec-d0a01e5f9bf5", null, "az2016", "GR_API"
         );
     }
 }
