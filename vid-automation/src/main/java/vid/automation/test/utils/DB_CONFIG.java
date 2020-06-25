7e6440acec6612467c366c326822ab779da327ff
Tschaen, Brendan <ctschaen@att.com>
2020-06-25 09:56:17 -0400

    Fix deprecated database names
    Issue-ID: VID-851
    Change-Id: I21ff8cad3460ca4ebdc0c65f129aa4c6e0dddc51
    Signed-off-by: Tschaen, Brendan <ctschaen@att.com>

--- vid-automation/src/main/java/vid/automation/test/utils/DB_CONFIG.java
+++ vid-automation/src/main/java/vid/automation/test/utils/DB_CONFIG.java
@@ -1,7 +1,7 @@
 package vid.automation.test.utils;
 
 public class DB_CONFIG {
-    public static String url = String.format("jdbc:mariadb://%s:%d/vid_portal",
+    public static String url = String.format("jdbc:mariadb://%s:%d/vid_openecomp_epsdk",
             System.getProperty("DB_HOST", System.getProperty("VID_HOST", "127.0.0.1" )),
             Integer.valueOf(System.getProperty("DB_PORT", "3306"))
     );
