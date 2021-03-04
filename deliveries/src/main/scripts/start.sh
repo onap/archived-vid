bf3ea468c24a0dfe9e67411db6f095febe4d32e8
Ikram Ikramullah <ikram@research.att.com>
2021-03-04 14:55:33 +0000

    Revert "Fix for VID-890"
    This reverts commit 62bb1f83ba6658184d23bbaa465eb80011de3b2b.
    Reason for revert: Reason for revert: This is blocking us from releasing the honolulu release - the change is much wider than initially thought of - parts of this change are still in the open in oom - this cannot go alone. See VID-890 for more details.
    
    Issue-ID: VID-890
    Change-Id: Ie923d34b9c1c39efeed3d93e955e454aab032f65
    Signed-off-by: Ikramullah, Ikram <ikram@research.att.com>

--- deliveries/src/main/scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set CATALINA_OPTS if not defined previously
-# Enables late-evaluation of env variables, such as VID_KEYSTORE_PASSWORD
-: "${CATALINA_OPTS:=-Dvid.keystore.password=${VID_KEYSTORE_PASSWORD} -Dvid.keyalias=vid@vid.onap.org -Dvid.keystore.filename=${VID_KEYSTORE_FILENAME} -Dcom.att.eelf.logging.file=logback.xml -Dcom.att.eelf.logging.path=/tmp/vid/}"
-echo "CATALINA_OPTS: ${CATALINA_OPTS}"
-export CATALINA_OPTS
-
-catalina.sh run
